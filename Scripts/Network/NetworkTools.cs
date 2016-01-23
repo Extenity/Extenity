6f6d4919cda6579c57a8c7e4a6881c281fb86c62
Can Baycay <can.baycay@gmail.com>
2016-01-23 16:34:44 +0200

    .

--- Scripts/Network/NetworkTools.cs
+++ Scripts/Network/NetworkTools.cs
@@ -78,7 +78,7 @@ public static class NetworkTools
 		{
 			using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
 			{
-				socket.Connect("10.0.0.0", 65530);
+				socket.Connect("1.0.0.0", 65530);
 				var endPoint = (IPEndPoint)socket.LocalEndPoint;
 				return endPoint.Address.ToString();
 			}
