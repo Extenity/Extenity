3cedf3ab08724eec227aff170a29a683433e3a0b
Can Baycay <can.baycay@gmail.com>
2016-04-07 02:31:28 +0300

    Add: ObjectEnabler

---
+++ Scripts/GameObject/ObjectEnabler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ObjectEnabler : MonoBehaviour
+{
+	public GameObject Object;
+
+	public void EnableObject(bool enable)
+	{
+		Object.SetActive(enable);
+	}
+
+	public void EnableObject()
+	{
+		Object.SetActive(true);
+	}
+
+	public void DisableObject()
+	{
+		Object.SetActive(false);
+	}
+}
