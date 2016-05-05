a6a0deca79da922a8e167f26852b710d40e77354
Can Baycay <can.baycay@gmail.com>
2016-05-05 22:33:46 +0300

    Add: IsVisibleFrom

--- Scripts/Camera/CameraTools.cs
+++ Scripts/Camera/CameraTools.cs
@@ -11,4 +11,88 @@ public static class CameraTools
 		}
 		return null;
 	}
+
+	#region IsVisibleFrom
+
+	public static bool IsCollidersVisibleFrom(this Transform transform, Vector3 eyePosition)
+	{
+		var colliders = transform.GetComponents<Collider>();
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].IsVisibleFrom(eyePosition))
+				return true;
+		}
+
+		for (int iChild = 0; iChild < transform.childCount; iChild++)
+		{
+			if (transform.GetChild(iChild).IsCollidersVisibleFrom(eyePosition))
+				return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsVisibleFrom(this Collider collider, Vector3 eyePosition)
+	{
+		return IsVisibleFrom(collider.bounds, eyePosition);
+	}
+
+	public static bool IsRenderersVisibleFrom(this Transform transform, Vector3 eyePosition)
+	{
+		var renderer = transform.GetComponent<Renderer>();
+		if (renderer != null)
+			if (renderer.IsVisibleFrom(eyePosition))
+				return true;
+
+		for (int iChild = 0; iChild < transform.childCount; iChild++)
+		{
+			if (transform.GetChild(iChild).IsRenderersVisibleFrom(eyePosition))
+				return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsVisibleFrom(this Renderer renderer, Vector3 eyePosition)
+	{
+		return IsVisibleFrom(renderer.bounds, eyePosition);
+	}
+
+	public static bool IsVisibleFrom(this Bounds bounds, Vector3 eyePosition)
+	{
+		var center = bounds.center;
+		var extents = bounds.extents;
+
+		if (!Physics.Linecast(center, eyePosition))
+			return true;
+
+		var corner = new Vector3(center.x - extents.x, center.y + extents.y, center.z - extents.z); // Front top left corner
+		if (!Physics.Linecast(corner, eyePosition))
+			return true;
+		corner.Set(center.x + extents.x, center.y + extents.y, center.z - extents.z); // Front top right corner
+		if (!Physics.Linecast(corner, eyePosition))
+			return true;
+		corner.Set(center.x - extents.x, center.y - extents.y, center.z - extents.z); // Front bottom left corner
+		if (!Physics.Linecast(corner, eyePosition))
+			return true;
+		corner.Set(center.x + extents.x, center.y - extents.y, center.z - extents.z); // Front bottom right corner
+		if (!Physics.Linecast(corner, eyePosition))
+			return true;
+		corner.Set(center.x - extents.x, center.y + extents.y, center.z + extents.z); // Back top left corner
+		if (!Physics.Linecast(corner, eyePosition))
+			return true;
+		corner.Set(center.x + extents.x, center.y + extents.y, center.z + extents.z); // Back top right corner
+		if (!Physics.Linecast(corner, eyePosition))
+			return true;
+		corner.Set(center.x - extents.x, center.y - extents.y, center.z + extents.z); // Back bottom left corner
+		if (!Physics.Linecast(corner, eyePosition))
+			return true;
+		corner.Set(center.x + extents.x, center.y - extents.y, center.z + extents.z); // Back bottom right corner
+		if (!Physics.Linecast(corner, eyePosition))
+			return true;
+
+		return false;
+	}
+
+	#endregion
 }
