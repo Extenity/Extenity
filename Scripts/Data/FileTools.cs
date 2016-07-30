06f337d771662819e7b5e5206496e7da830c64da
Can Baycay <can.baycay@gmail.com>
2016-07-30 13:11:44 +0300

    Improved file content comparison.

--- Scripts/Data/FileTools.cs
+++ Scripts/Data/FileTools.cs
@@ -1,8 +1,6 @@
 using System;
 using System.ComponentModel;
 using System.IO;
-using UnityEngine;
-using System.Collections;
 
 public static class FileTools
 {
@@ -207,61 +205,33 @@ public static class FileTools
 	{
 		string fullPath1 = Path.GetFullPath(path1);
 		string fullPath2 = Path.GetFullPath(path2);
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
 		return 0 == String.Compare(
 			fullPath1,
 			fullPath2,
 			StringComparison.InvariantCultureIgnoreCase);
+#else
+		// Make sure casing won't be a problem for other platforms
+		throw new NotImplementedException();
+#endif
 	}
 
-	public static bool FileCompare(string filePath1, string filePath2)
+	/// <summary>
+	/// Compares if two file contents are the same. First it checks for file sizes and immediatelly tells if file sizes are different. Then it checks byte by byte until a difference found.
+	/// </summary>
+	/// <returns>True if files are the same.</returns>
+	public static bool CompareFileContents(this FileInfo fileInfo1, FileInfo fileInfo2)
 	{
-		string fullPath1 = Path.GetFullPath(filePath1);
-		string fullPath2 = Path.GetFullPath(filePath2);
-
-		// Determine if the same file was referenced two times.
-		if (fullPath1 == fullPath2)
-		{
-			// Return true to indicate that the files are the same.
-			return true;
-		}
-
-		// Open the two files.
-		FileStream fs1 = File.OpenRead(fullPath1);
-		FileStream fs2 = File.OpenRead(fullPath2);
-
-		// Check the file sizes. If they are not the same, the files 
-		// are not the same.
-		if (fs1.Length != fs2.Length)
-		{
-			// Close the file
-			fs1.Close();
-			fs2.Close();
-
-			// Return false to indicate files are different
+		if (fileInfo1.Length != fileInfo2.Length)
 			return false;
-		}
 
-		// Read and compare a byte from each file until either a
-		// non-matching set of bytes is found or until the end of
-		// file1 is reached.
-		int file1byte;
-		int file2byte;
-		do
+		using (var fileStream1 = fileInfo1.OpenRead())
 		{
-			// Read one byte from each file.
-			file1byte = fs1.ReadByte();
-			file2byte = fs2.ReadByte();
+			using (var fileStream2 = fileInfo2.OpenRead())
+			{
+				return fileStream1.CompareStreamContents(fileStream2);
+			}
 		}
-		while ((file1byte == file2byte) && (file1byte != -1));
-
-		// Close the files.
-		fs1.Close();
-		fs2.Close();
-
-		// Return the success of the comparison. "file1byte" is 
-		// equal to "file2byte" at this point only if the files are 
-		// the same.
-		return ((file1byte - file2byte) == 0);
 	}
 
 	#endregion
---
+++ Scripts/Data/StreamTools.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+public static class StreamTools
+{
+
+	public static bool CompareStreamContents(this Stream stream1, Stream stream2)
+	{
+		const int bufferSize = 1024 * sizeof(Int64);
+		var buffer1 = new byte[bufferSize];
+		var buffer2 = new byte[bufferSize];
+
+		while (true)
+		{
+			int count1 = stream1.Read(buffer1, 0, bufferSize);
+			int count2 = stream2.Read(buffer2, 0, bufferSize);
+
+			if (count1 != count2)
+			{
+				return false;
+			}
+
+			if (count1 == 0)
+			{
+				return true;
+			}
+
+			int iterations = (int)Math.Ceiling((double)count1 / sizeof(Int64));
+			for (int i = 0; i < iterations; i++)
+			{
+				if (BitConverter.ToInt64(buffer1, i * sizeof(Int64)) !=
+					BitConverter.ToInt64(buffer2, i * sizeof(Int64)))
+				{
+					return false;
+				}
+			}
+		}
+	}
+
+}
