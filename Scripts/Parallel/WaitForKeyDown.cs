c897adedc5954c690fe5ad98f7aee5df809c8908
Can Baycay <can.baycay@gmail.com>
2016-08-26 20:00:10 +0300

    .

--- Scripts/Parallel/WaitForKeyDown.cs
+++ Scripts/Parallel/WaitForKeyDown.cs
@@ -6,18 +6,32 @@ namespace Extenity.Parallel
 	public class WaitForKeyDown : CustomYieldInstruction
 	{
 		public KeyCode KeyCode { get; set; }
+		public KeyCode ResetKeyCode { get; set; }
 		private bool IsKeyDown { get; set; }
 
-		public WaitForKeyDown(KeyCode keyCode)
+		public WaitForKeyDown(KeyCode keyCode, KeyCode resetKeyCode = KeyCode.None)
 		{
 			KeyCode = keyCode;
+			ResetKeyCode = resetKeyCode;
 		}
 
 		public override bool keepWaiting
 		{
 			get
 			{
+				if (KeyCode == KeyCode.None)
+					return false;
+				if (Input.GetKey(ResetKeyCode))
+				{
+					KeyCode = KeyCode.None;
+					return false;
+				}
+
 				var keepWaiting = true;
+				if (!Input.GetKey(KeyCode)) // We would miss Key Up event if this yield instruction is not called in every update. So this is a backup to detect key release.
+				{
+					IsKeyDown = false;
+				}
 				if (Input.GetKeyDown(KeyCode))
 				{
 					if (!IsKeyDown)
