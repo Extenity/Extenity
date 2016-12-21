6691a0704f9972520f825f0fddfe303a1eb4ab74
Can Baycay <can.baycay@gmail.com>
2016-12-21 05:28:51 +0300

    .

---
+++ Scripts/Flow/Pauser.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Extenity.Flow
+{
+
+	public class PauseHandler
+	{
+		public readonly int ID;
+		public readonly Pauser Pauser;
+		public readonly string Description;
+
+		private static int _LastGivenID = 100;
+
+		internal PauseHandler(Pauser pauser, string description)
+		{
+			ID = ++_LastGivenID;
+			Pauser = pauser;
+			Description = description;
+		}
+
+		/// <summary>
+		/// Resumes the related pause call. It's safe to call this more than once. Only first call will be processed and others will be silently ignored.
+		/// </summary>
+		public void Resume()
+		{
+			Pauser.Resume(this);
+		}
+	}
+
+	public class Pauser
+	{
+		#region Initialization
+
+		public Pauser(string id)
+		{
+			ID = id;
+		}
+
+		#endregion
+
+		#region Metadata - ID
+
+		public readonly string ID;
+
+		#endregion
+
+		#region Active Pause Handlers
+
+		private List<PauseHandler> ActivePauseHandlers = new List<PauseHandler>(10);
+
+		public string[] GetActivePauseHandlerDescriptions()
+		{
+			var list = new string[ActivePauseHandlers.Count];
+			for (int i = 0; i < ActivePauseHandlers.Count; i++)
+			{
+				list[i] = ActivePauseHandlers[i].Description;
+			}
+			return list;
+		}
+
+		#endregion
+
+		#region Pause / Resume
+
+		public bool IsPaused { get { return ActivePauseHandlers.Count > 0; } }
+		public int PauseCount { get { return ActivePauseHandlers.Count; } }
+
+		public PauseHandler Pause(string description)
+		{
+			if (IsLoggingEnabled)
+			{
+				Debug.LogFormat("Pausing for '{0}' in Pauser '{1}'.", description, ID);
+			}
+
+			var handler = new PauseHandler(this, description);
+			ActivePauseHandlers.Add(handler);
+			return handler;
+		}
+
+		public bool Resume(PauseHandler handler)
+		{
+			if (handler == null)
+			{
+				Debug.LogErrorFormat("Tried to resume Pauser '{0}' but pause handler is null.", ID);
+				return false;
+			}
+
+			var resumed = ActivePauseHandlers.Remove(handler);
+
+			if (IsLoggingEnabled)
+			{
+				if (resumed)
+				{
+					Debug.LogFormat("Resuming '{0}' in Pauser '{1}'.", handler.Description, ID);
+				}
+				else
+				{
+					Debug.LogFormat("Resuming '{0}' (though it was already resumed or cut loose) in Pauser '{1}'.", handler.Description, ID);
+				}
+			}
+
+			return resumed;
+		}
+
+		/// <summary>
+		/// Overrides all pause  handlers and removes all handlers from active handler list, making them not function anymore. It's safe to call resume on these loose pause handlers. These calls will be ignored silently.
+		/// </summary>
+		public void ResumeAll()
+		{
+			if (IsPaused)
+			{
+				if (IsLoggingEnabled)
+				{
+					Debug.LogFormat("Resuming all {0} handler(s) '{1}' in Pauser '{2}'.",
+						PauseCount,
+						string.Join(", ", GetActivePauseHandlerDescriptions()),
+						ID);
+				}
+
+				ActivePauseHandlers.Clear();
+			}
+			else
+			{
+				if (IsLoggingEnabled)
+				{
+					Debug.LogFormat("Resuming all handlers (though there were none) in Pauser '{0}'.", ID);
+				}
+			}
+		}
+
+		#endregion
+
+		#region Logging
+
+		public bool IsLoggingEnabled = true;
+
+		#endregion
+	}
+
+}
