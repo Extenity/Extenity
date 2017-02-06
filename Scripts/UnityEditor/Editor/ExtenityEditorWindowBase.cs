3120929425a8ca415ca07692b4038f407ed1becd
Can Baycay <can.baycay@gmail.com>
2017-02-07 00:24:37 +0300

    Add: Robust icon handling inside ExtenityEditorWindowBase.

--- Scripts/UnityEditor/Editor/ExtenityEditorWindowBase.cs
+++ Scripts/UnityEditor/Editor/ExtenityEditorWindowBase.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEditor;
 
 public abstract class ExtenityEditorWindowBase : EditorWindow
@@ -9,8 +10,34 @@ public abstract class ExtenityEditorWindowBase : EditorWindow
 
 	protected void OnGUI()
 	{
+		if (RefreshTitleContentInNextRender)
+		{
+			titleContent = new GUIContent(_Title, _IconRetriever != null ? _IconRetriever() : null);
+			_Title = null;
+			_IconRetriever = null;
+			RefreshTitleContentInNextRender = false;
+		}
+
 		CalculateRightMouseButtonScrolling();
 		OnGUIDerived();
+	}
+
+	#endregion
+
+	#region Title And Icon
+
+	private string _Title;
+	private Func<Texture2D> _IconRetriever;
+	private bool RefreshTitleContentInNextRender;
+
+	/// <summary>
+	/// Call this inside OnEnable.
+	/// </summary>
+	public void SetTitleAndIcon(string title, Func<Texture2D> iconRetriever)
+	{
+		_Title = title;
+		_IconRetriever = iconRetriever;
+		RefreshTitleContentInNextRender = true; // This will prevent any possibilities of losing window icon. This could happen on relaunching the editor while window was left open.
 	}
 
 	#endregion
@@ -23,13 +50,14 @@ public abstract class ExtenityEditorWindowBase : EditorWindow
 
 	#region Scroll Window With Right Mouse Button
 
+	public bool IsRightMouseButtonScrollingEnabled = false;
 	private bool WasScrollingWithRightMouseButton;
 
 	private void CalculateRightMouseButtonScrolling()
 	{
 		if (Event.current.isMouse)
 		{
-			if (Event.current.button == 1 && Event.current.type == EventType.mouseDrag)
+			if (IsRightMouseButtonScrollingEnabled && Event.current.button == 1 && Event.current.type == EventType.MouseDrag)
 			{
 				ScrollPosition -= Event.current.delta;
 
@@ -40,7 +68,7 @@ public abstract class ExtenityEditorWindowBase : EditorWindow
 			}
 
 			// Prevent any right click events if right click is used for scrolling.
-			if (WasScrollingWithRightMouseButton && Event.current.type == EventType.mouseUp)
+			if (WasScrollingWithRightMouseButton && Event.current.type == EventType.MouseUp)
 			{
 				WasScrollingWithRightMouseButton = false;
 				Event.current.Use();
