f31337c921c1cfb425baf59f278aa3c7b0161b56
Can Baycay <can.baycay@gmail.com>
2017-03-10 10:44:27 +0300

    Add: OutsideClickDetector

---
+++ Scripts/GUI/OutsideClickDetector.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Extenity.UIToolbox
+{
+
+	public class OutsideClickDetector : MonoBehaviour, IPointerClickHandler
+	{
+		public UnityEvent OnClickedOutside = new UnityEvent();
+
+		public void OnPointerClick(PointerEventData eventData)
+		{
+			OnClickedOutside.Invoke();
+			eventData.Use();
+		}
+	}
+
+}
