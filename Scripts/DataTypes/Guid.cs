2337c5d08cbf4bbf550c03e8ee47a9f5df680d7d
Can Baycay <can.baycay@gmail.com>
2017-03-15 06:36:49 +0300

    Add: Guid.NewPseudoGuid

--- Scripts/DataTypes/Guid.cs
+++ Scripts/DataTypes/Guid.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using UnityEngine;
-using System.Collections;
-using System.Runtime.Serialization;
+using Random = UnityEngine.Random;
 
 namespace Extenity.DataTypes
 {
@@ -66,6 +64,21 @@ namespace Extenity.DataTypes
 		public static Guid NewGuid()
 		{
 			return new Guid(System.Guid.NewGuid());
+		}
+
+		/// <summary>
+		/// Generates a guid that depends on UnityEngine.Random's seed. Makes sure the generated guid is always the same if you give it the same seed. 
+		/// Note that this method of generating a guid is not reliable to be unique. The randomness properties of the guid is actually the same as the 
+		/// UnityEngine.Random (probably 32 bit seed).
+		/// </summary>
+		public static Guid NewPseudoGuid()
+		{
+			var data = new byte[16];
+			for (int i = 0; i < 16; i++)
+			{
+				data[i] = (byte)(Random.value * 256f);
+			}
+			return new Guid { Data = data };
 		}
 
 		#endregion
