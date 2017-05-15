09a45191560b0de79c6ff133a36d3cd7a0d4f75e
Can Baycay <can.baycay@gmail.com>
2017-05-15 23:23:07 +0300

    Refactor: Namespace.

--- Scripts/CodeSnippets/Editor/CodeSnippets.cs
+++ Scripts/CodeSnippets/Editor/CodeSnippets.cs
@@ -6,18 +6,21 @@ using System.IO;
 using System.Linq;
 using Extenity.AssetToolbox;
 
-public static class CodeSnippets
+namespace Extenity.CodeSnippetsToolbox
 {
+
+	public static class CodeSnippets
+	{
 #pragma warning disable 414
 
-	#region Snippet - Main Script
+		#region Snippet - Main Script
 
-	private static readonly SnippetInfo MainScript = new SnippetInfo()
-	{
-		Name = "Main",
-		Path = "__NAME__.cs",
-		Properties = new[] { "__NAME__" },
-		FileContent =
+		private static readonly SnippetInfo MainScript = new SnippetInfo()
+		{
+			Name = "Main",
+			Path = "__NAME__.cs",
+			Properties = new[] { "__NAME__" },
+			FileContent =
 @"using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -49,18 +52,18 @@ public class __NAME__ : MonoBehaviour
 	#endregion
 }
 "
-	};
+		};
 
-	#endregion
+		#endregion
 
-	#region Snippet - Main Script (Namespace)
+		#region Snippet - Main Script (Namespace)
 
-	private static readonly SnippetInfo NamespacedMainScript = new SnippetInfo()
-	{
-		Name = "NamespacedMain",
-		Path = "__NAME__.cs",
-		Properties = new[] { "__NAME__", "__NAMESPACE__" },
-		FileContent =
+		private static readonly SnippetInfo NamespacedMainScript = new SnippetInfo()
+		{
+			Name = "NamespacedMain",
+			Path = "__NAME__.cs",
+			Properties = new[] { "__NAME__", "__NAMESPACE__" },
+			FileContent =
 @"using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -97,18 +100,18 @@ namespace __NAMESPACE__
 
 }
 "
-	};
+		};
 
-	#endregion
+		#endregion
 
-	#region Snippet - Inspector Script
+		#region Snippet - Inspector Script
 
-	private static readonly SnippetInfo InspectorScript = new SnippetInfo()
-	{
-		Name = "Inspector",
-		Path = "Editor/__NAME__Inspector.cs",
-		Properties = new[] { "__NAME__" },
-		FileContent =
+		private static readonly SnippetInfo InspectorScript = new SnippetInfo()
+		{
+			Name = "Inspector",
+			Path = "Editor/__NAME__Inspector.cs",
+			Properties = new[] { "__NAME__" },
+			FileContent =
 @"using UnityEngine;
 using UnityEditor;
 using System.Collections;
@@ -130,18 +133,18 @@ public class __NAME__Inspector : ExtenityEditorBase<__NAME__>
 	}
 }
 "
-	};
+		};
 
-	#endregion
+		#endregion
 
-	#region Snippet - Inspector Script (Namespace)
+		#region Snippet - Inspector Script (Namespace)
 
-	private static readonly SnippetInfo NamespacedInspectorScript = new SnippetInfo()
-	{
-		Name = "NamespacedInspector",
-		Path = "Editor/__NAME__Inspector.cs",
-		Properties = new[] { "__NAME__", "__NAMESPACE__" },
-		FileContent =
+		private static readonly SnippetInfo NamespacedInspectorScript = new SnippetInfo()
+		{
+			Name = "NamespacedInspector",
+			Path = "Editor/__NAME__Inspector.cs",
+			Properties = new[] { "__NAME__", "__NAMESPACE__" },
+			FileContent =
 @"using UnityEngine;
 using UnityEditor;
 using System.Collections;
@@ -168,207 +171,209 @@ namespace __NAMESPACE__
 
 }
 "
-	};
+		};
 
-	#endregion
+		#endregion
 
-	#region Snippet Groups
+		#region Snippet Groups
 
-	private static SnippetGroup ScriptWithInspectorGroup = new SnippetGroup()
-	{
-		MainFileExtension = "cs",
-		SnippetNames = new List<string> { "NamespacedMain", "NamespacedInspector" }
-	};
+		private static SnippetGroup ScriptWithInspectorGroup = new SnippetGroup()
+		{
+			MainFileExtension = "cs",
+			SnippetNames = new List<string> { "NamespacedMain", "NamespacedInspector" }
+		};
 
-	private static SnippetGroup ScriptOnlyGroup = new SnippetGroup()
-	{
-		MainFileExtension = "cs",
-		SnippetNames = new List<string> { "NamespacedMain" }
-	};
+		private static SnippetGroup ScriptOnlyGroup = new SnippetGroup()
+		{
+			MainFileExtension = "cs",
+			SnippetNames = new List<string> { "NamespacedMain" }
+		};
 
-	private static SnippetGroup InspectorOnlyGroup = new SnippetGroup()
-	{
-		MainFileExtension = "cs",
-		SnippetNames = new List<string> { "NamespacedInspector" }
-	};
+		private static SnippetGroup InspectorOnlyGroup = new SnippetGroup()
+		{
+			MainFileExtension = "cs",
+			SnippetNames = new List<string> { "NamespacedInspector" }
+		};
 
-	#endregion
+		#endregion
 
 #pragma warning restore 414
 
-	#region Snippet Info
+		#region Snippet Info
 
-	private class SnippetInfo
-	{
-		public string Name;
-		public string Path;
-		public string[] Properties;
-		public string FileContent;
-
-		public SnippetInfo()
+		private class SnippetInfo
 		{
-			_RegisterSnippetInfo(this);
-		}
-	}
+			public string Name;
+			public string Path;
+			public string[] Properties;
+			public string FileContent;
 
-	#endregion
-
-	#region All Snippet Infos
-
-	private static List<SnippetInfo> AllSnippetInfos;
-
-	private static void _RegisterSnippetInfo(SnippetInfo snippetInfo)
-	{
-		if (AllSnippetInfos == null)
-		{
-			AllSnippetInfos = new List<SnippetInfo>();
-		}
-		AllSnippetInfos.Add(snippetInfo);
-	}
-
-	private static SnippetInfo GetSnippetInfo(string snippetName)
-	{
-		if (AllSnippetInfos == null)
-			return null;
-
-		return AllSnippetInfos.FirstOrDefault(snippetInfo => snippetInfo.Name == snippetName);
-	}
-
-	#endregion
-
-	#region Snippet Group
-
-	private class SnippetGroup
-	{
-		public List<string> SnippetNames;
-		public string MainFileExtension;
-
-		public List<string> GetAllSnippetFilePaths(string baseDirectory, Dictionary<string, string> macroDefinitions)
-		{
-			var list = new List<string>(SnippetNames.Count);
-			foreach (var snippetName in SnippetNames)
+			public SnippetInfo()
 			{
-				var snippetInfo = GetSnippetInfo(snippetName);
-				var processedPath = ProcessMacrosInText(snippetInfo.Path, macroDefinitions);
-				var combinedPath = Path.Combine(baseDirectory, processedPath);
-				list.Add(combinedPath);
+				_RegisterSnippetInfo(this);
 			}
-			return list;
 		}
 
-		public void CreateAllSnippetFiles(string baseDirectory, Dictionary<string, string> macroDefinitions)
+		#endregion
+
+		#region All Snippet Infos
+
+		private static List<SnippetInfo> AllSnippetInfos;
+
+		private static void _RegisterSnippetInfo(SnippetInfo snippetInfo)
 		{
-			foreach (var snippetName in SnippetNames)
+			if (AllSnippetInfos == null)
 			{
-				try
+				AllSnippetInfos = new List<SnippetInfo>();
+			}
+			AllSnippetInfos.Add(snippetInfo);
+		}
+
+		private static SnippetInfo GetSnippetInfo(string snippetName)
+		{
+			if (AllSnippetInfos == null)
+				return null;
+
+			return AllSnippetInfos.FirstOrDefault(snippetInfo => snippetInfo.Name == snippetName);
+		}
+
+		#endregion
+
+		#region Snippet Group
+
+		private class SnippetGroup
+		{
+			public List<string> SnippetNames;
+			public string MainFileExtension;
+
+			public List<string> GetAllSnippetFilePaths(string baseDirectory, Dictionary<string, string> macroDefinitions)
+			{
+				var list = new List<string>(SnippetNames.Count);
+				foreach (var snippetName in SnippetNames)
 				{
 					var snippetInfo = GetSnippetInfo(snippetName);
 					var processedPath = ProcessMacrosInText(snippetInfo.Path, macroDefinitions);
 					var combinedPath = Path.Combine(baseDirectory, processedPath);
-					var combinedPathDirectory = Path.GetDirectoryName(combinedPath);
-					var processedFileContent = ProcessMacrosInText(snippetInfo.FileContent, macroDefinitions);
-
-					if (!string.IsNullOrEmpty(combinedPathDirectory))
-					{
-						if (!Directory.Exists(combinedPathDirectory))
-						{
-							Directory.CreateDirectory(combinedPathDirectory);
-						}
-					}
-					File.WriteAllText(combinedPath, processedFileContent);
+					list.Add(combinedPath);
 				}
-				catch (Exception e)
+				return list;
+			}
+
+			public void CreateAllSnippetFiles(string baseDirectory, Dictionary<string, string> macroDefinitions)
+			{
+				foreach (var snippetName in SnippetNames)
 				{
-					Debug.LogError("Could not create snippet file for '" + snippetName + "' snippet. Exception: " + e);
+					try
+					{
+						var snippetInfo = GetSnippetInfo(snippetName);
+						var processedPath = ProcessMacrosInText(snippetInfo.Path, macroDefinitions);
+						var combinedPath = Path.Combine(baseDirectory, processedPath);
+						var combinedPathDirectory = Path.GetDirectoryName(combinedPath);
+						var processedFileContent = ProcessMacrosInText(snippetInfo.FileContent, macroDefinitions);
+
+						if (!string.IsNullOrEmpty(combinedPathDirectory))
+						{
+							if (!Directory.Exists(combinedPathDirectory))
+							{
+								Directory.CreateDirectory(combinedPathDirectory);
+							}
+						}
+						File.WriteAllText(combinedPath, processedFileContent);
+					}
+					catch (Exception e)
+					{
+						Debug.LogError("Could not create snippet file for '" + snippetName + "' snippet. Exception: " + e);
+					}
 				}
 			}
 		}
-	}
 
-	#endregion
+		#endregion
 
-	#region Macros
+		#region Macros
 
-	private static Dictionary<string, string> CreateMacroDefinitions(string scriptName)
-	{
-		return new Dictionary<string, string>
+		private static Dictionary<string, string> CreateMacroDefinitions(string scriptName)
+		{
+			return new Dictionary<string, string>
 			{
 				{"__NAME__", scriptName}
 			};
-	}
-
-	private static string ProcessMacrosInText(string text, Dictionary<string, string> macroDefinitions)
-	{
-		foreach (var macroDefinition in macroDefinitions)
-		{
-			text = text.Replace(macroDefinition.Key, macroDefinition.Value);
 		}
-		return text;
-	}
 
-	#endregion
-
-	#region File Operations
-
-	private static bool CheckIfAnySnippetFileAlreadyExists(List<string> filePaths)
-	{
-		var found = false;
-		foreach (var filePath in filePaths)
+		private static string ProcessMacrosInText(string text, Dictionary<string, string> macroDefinitions)
 		{
-			if (File.Exists(filePath))
+			foreach (var macroDefinition in macroDefinitions)
 			{
-				found = true;
-				Debug.LogError("Could not create snippet file because a file already exists at path '" + filePath + "'.");
+				text = text.Replace(macroDefinition.Key, macroDefinition.Value);
 			}
+			return text;
 		}
-		return found;
+
+		#endregion
+
+		#region File Operations
+
+		private static bool CheckIfAnySnippetFileAlreadyExists(List<string> filePaths)
+		{
+			var found = false;
+			foreach (var filePath in filePaths)
+			{
+				if (File.Exists(filePath))
+				{
+					found = true;
+					Debug.LogError("Could not create snippet file because a file already exists at path '" + filePath + "'.");
+				}
+			}
+			return found;
+		}
+
+		#endregion
+
+		#region Create Snippet
+
+		private static void CreateSnippet(SnippetGroup snippetGroup)
+		{
+			var path = EditorUtility.SaveFilePanel("Give a name to your script", AssetTools.GetSelectedPathOrAssetRootPath(), "", snippetGroup.MainFileExtension);
+
+			if (string.IsNullOrEmpty(path))
+				return;
+
+			var scriptName = Path.GetFileNameWithoutExtension(path);
+			var baseDirectory = Path.GetDirectoryName(path);
+			var macroDefinitions = CreateMacroDefinitions(scriptName);
+			var snippetFilePaths = snippetGroup.GetAllSnippetFilePaths(baseDirectory, macroDefinitions);
+
+			if (CheckIfAnySnippetFileAlreadyExists(snippetFilePaths))
+				return;
+
+			snippetGroup.CreateAllSnippetFiles(baseDirectory, macroDefinitions);
+
+			AssetDatabase.Refresh();
+		}
+
+		#endregion
+
+		#region Menu Commands
+
+		[MenuItem("Snippets/Create Script with Inspector")]
+		public static void CreateScriptWithInspector()
+		{
+			CreateSnippet(ScriptWithInspectorGroup);
+		}
+
+		[MenuItem("Snippets/Create Script")]
+		public static void CreateScript()
+		{
+			CreateSnippet(ScriptOnlyGroup);
+		}
+
+		[MenuItem("Snippets/Create Inspector")]
+		public static void CreateInspector()
+		{
+			CreateSnippet(InspectorOnlyGroup);
+		}
+
+		#endregion
 	}
 
-	#endregion
-
-	#region Create Snippet
-
-	private static void CreateSnippet(SnippetGroup snippetGroup)
-	{
-		var path = EditorUtility.SaveFilePanel("Give a name to your script", AssetTools.GetSelectedPathOrAssetRootPath(), "", snippetGroup.MainFileExtension);
-
-		if (string.IsNullOrEmpty(path))
-			return;
-
-		var scriptName = Path.GetFileNameWithoutExtension(path);
-		var baseDirectory = Path.GetDirectoryName(path);
-		var macroDefinitions = CreateMacroDefinitions(scriptName);
-		var snippetFilePaths = snippetGroup.GetAllSnippetFilePaths(baseDirectory, macroDefinitions);
-
-		if (CheckIfAnySnippetFileAlreadyExists(snippetFilePaths))
-			return;
-
-		snippetGroup.CreateAllSnippetFiles(baseDirectory, macroDefinitions);
-
-		AssetDatabase.Refresh();
-	}
-
-	#endregion
-
-	#region Menu Commands
-
-	[MenuItem("Snippets/Create Script with Inspector")]
-	public static void CreateScriptWithInspector()
-	{
-		CreateSnippet(ScriptWithInspectorGroup);
-	}
-
-	[MenuItem("Snippets/Create Script")]
-	public static void CreateScript()
-	{
-		CreateSnippet(ScriptOnlyGroup);
-	}
-
-	[MenuItem("Snippets/Create Inspector")]
-	public static void CreateInspector()
-	{
-		CreateSnippet(InspectorOnlyGroup);
-	}
-
-	#endregion
 }
--- Scripts/CodeSnippets/Editor/CodeSnippetsWindow.cs
+++ Scripts/CodeSnippets/Editor/CodeSnippetsWindow.cs
@@ -8,298 +8,303 @@ using Extenity.ApplicationToolbox;
 using Extenity.DataTypes;
 using UnityEditor;
 
-public class CodeSnippetsWindow : ExtenityEditorWindowBase
+namespace Extenity.CodeSnippetsToolbox
 {
-	#region Initialization
 
-	protected void Awake()
+	public class CodeSnippetsWindow : ExtenityEditorWindowBase
 	{
-		EditorApplication.update += Update;
+		#region Initialization
 
-		StartNamespaceGathering();
-	}
-
-	#endregion
-
-	#region Deinitialization
-
-	protected void OnDestroy()
-	{
-		EditorApplication.update -= Update;
-
-		CancelNamespaceGathering();
-	}
-
-	#endregion
-
-	#region Update
-
-	protected void Update()
-	{
-		UpdateNamespaceGathering();
-	}
-
-	#endregion
-
-	#region Configuration
-
-	private string BuildConfigurationKey<T>(Expression<Func<T>> propertyLambda)
-	{
-		return "CodeSnippets." + ApplicationTools.AsciiProductName + "." + TypeTools.GetPropertyName(propertyLambda);
-	}
-
-	private string _Namespace;
-	public string Namespace
-	{
-		get
+		protected void Awake()
 		{
-			if (_Namespace == null)
-			{
-				_Namespace = EditorPrefs.GetString(BuildConfigurationKey(() => Namespace), "");
-			}
-			return _Namespace;
-		}
-		set
-		{
-			if (_Namespace == value)
-				return;
+			EditorApplication.update += Update;
 
-			_Namespace = value;
-			EditorPrefs.SetString(BuildConfigurationKey(() => Namespace), _Namespace);
-		}
-	}
-
-	#endregion
-
-	#region GUI
-
-	protected override void OnGUIDerived()
-	{
-		EditorGUILayout.BeginVertical();
-
-		Namespace = EditorGUILayout.TextField("Namespace (can be empty)", Namespace);
-
-		GUILayout.Label("Namespaces: " + NamespacesCount);
-		EditorGUILayoutTools.ProgressBar("Progress", NamespaceGatheringProgress);
-
-		for (int i = 0; i < Namespaces.Count; i++)
-		{
-			var ns = Namespaces[i];
-			if (GUILayout.Button(ns))
-			{
-				Namespace = ns;
-			}
+			StartNamespaceGathering();
 		}
 
-		//ClassDirectoryPath = EditorGUILayout.TextField("Namespace (can be empty)", Namespace);
+		#endregion
 
-		EditorGUILayout.EndVertical();
-	}
+		#region Deinitialization
 
-	#endregion
-
-	#region All Namespaces In Project
-
-	public List<string> Namespaces;
-	public int NamespacesCount { get { return Namespaces == null ? 0 : Namespaces.Count; } }
-	private List<string> NamespacesListToCheckAvailability;
-	public bool IsGatheringNamespaces { get; private set; }
-	public float NamespaceGatheringProgress { get; private set; }
-	private Thread NamespaceGatheringThread;
-	private ThreadStopper NamespaceGatheringThreadStopper;
-	private static readonly string NamespaceContentSearchString = "namespace ";
-	private static readonly object NamespaceListLocker = new object();
-	private bool SignalToFinalizeNamespaceGathering { get; set; }
-	private bool SignalToRefreshGUI { get; set; }
-
-	private class ThreadStopper
-	{
-		public bool IsSignaledToStop;
-	}
-
-	public void StartNamespaceGathering()
-	{
-		if (IsGatheringNamespaces)
+		protected void OnDestroy()
 		{
+			EditorApplication.update -= Update;
+
 			CancelNamespaceGathering();
 		}
 
-		IsGatheringNamespaces = true;
-		SignalToFinalizeNamespaceGathering = false;
-		NamespacesListToCheckAvailability = Namespaces.Clone();
-		NamespaceGatheringProgress = 0f;
+		#endregion
 
-		// Sort initial list (if there is any)
-		SortNamespaces();
+		#region Update
 
-		// Create thread
+		protected void Update()
 		{
-			NamespaceGatheringThreadStopper = new ThreadStopper();
-			NamespaceGatheringThread = new Thread(NamespaceGatheringThreadMethod);
-			NamespaceGatheringThread.Start(NamespaceGatheringThreadStopper);
-		}
-	}
-
-	private void UpdateNamespaceGathering()
-	{
-		if (SignalToFinalizeNamespaceGathering)
-		{
-			FinalizeNamespaceGathering();
+			UpdateNamespaceGathering();
 		}
 
-		if (SignalToRefreshGUI)
+		#endregion
+
+		#region Configuration
+
+		private string BuildConfigurationKey<T>(Expression<Func<T>> propertyLambda)
 		{
-			RefreshNamespaceGatheringGUI();
+			return "CodeSnippets." + ApplicationTools.AsciiProductName + "." + TypeTools.GetPropertyName(propertyLambda);
 		}
-	}
 
-	private void RefreshNamespaceGatheringGUI()
-	{
-		Repaint();
-	}
-
-	private void NamespaceGatheringThreadMethod(object threadStopperObject)
-	{
-		try
+		private string _Namespace;
+		public string Namespace
 		{
-			var threadStopper = (ThreadStopper)threadStopperObject;
-
-			if (threadStopper.IsSignaledToStop)
-				return;
-
-			var path = Application.dataPath;
-			var sourceFiles = Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories);
-
-			if (sourceFiles != null)
+			get
 			{
-				// Look for namespaces in all files
-				for (int i = 0; i < sourceFiles.Length; i++)
+				if (_Namespace == null)
 				{
-					// Read file content
-					var sourceFile = sourceFiles[i];
-					var fileContent = File.ReadAllText(sourceFile);
+					_Namespace = EditorPrefs.GetString(BuildConfigurationKey(() => Namespace), "");
+				}
+				return _Namespace;
+			}
+			set
+			{
+				if (_Namespace == value)
+					return;
 
-					if (threadStopper.IsSignaledToStop)
-						return;
+				_Namespace = value;
+				EditorPrefs.SetString(BuildConfigurationKey(() => Namespace), _Namespace);
+			}
+		}
 
-					// Quickly search for namespace in file
-					var foundNamespaceIndex = 0;
-					while (true)
+		#endregion
+
+		#region GUI
+
+		protected override void OnGUIDerived()
+		{
+			EditorGUILayout.BeginVertical();
+
+			Namespace = EditorGUILayout.TextField("Namespace (can be empty)", Namespace);
+
+			GUILayout.Label("Namespaces: " + NamespacesCount);
+			EditorGUILayoutTools.ProgressBar("Progress", NamespaceGatheringProgress);
+
+			for (int i = 0; i < Namespaces.Count; i++)
+			{
+				var ns = Namespaces[i];
+				if (GUILayout.Button(ns))
+				{
+					Namespace = ns;
+				}
+			}
+
+			//ClassDirectoryPath = EditorGUILayout.TextField("Namespace (can be empty)", Namespace);
+
+			EditorGUILayout.EndVertical();
+		}
+
+		#endregion
+
+		#region All Namespaces In Project
+
+		public List<string> Namespaces;
+		public int NamespacesCount { get { return Namespaces == null ? 0 : Namespaces.Count; } }
+		private List<string> NamespacesListToCheckAvailability;
+		public bool IsGatheringNamespaces { get; private set; }
+		public float NamespaceGatheringProgress { get; private set; }
+		private Thread NamespaceGatheringThread;
+		private ThreadStopper NamespaceGatheringThreadStopper;
+		private static readonly string NamespaceContentSearchString = "namespace ";
+		private static readonly object NamespaceListLocker = new object();
+		private bool SignalToFinalizeNamespaceGathering { get; set; }
+		private bool SignalToRefreshGUI { get; set; }
+
+		private class ThreadStopper
+		{
+			public bool IsSignaledToStop;
+		}
+
+		public void StartNamespaceGathering()
+		{
+			if (IsGatheringNamespaces)
+			{
+				CancelNamespaceGathering();
+			}
+
+			IsGatheringNamespaces = true;
+			SignalToFinalizeNamespaceGathering = false;
+			NamespacesListToCheckAvailability = Namespaces.Clone();
+			NamespaceGatheringProgress = 0f;
+
+			// Sort initial list (if there is any)
+			SortNamespaces();
+
+			// Create thread
+			{
+				NamespaceGatheringThreadStopper = new ThreadStopper();
+				NamespaceGatheringThread = new Thread(NamespaceGatheringThreadMethod);
+				NamespaceGatheringThread.Start(NamespaceGatheringThreadStopper);
+			}
+		}
+
+		private void UpdateNamespaceGathering()
+		{
+			if (SignalToFinalizeNamespaceGathering)
+			{
+				FinalizeNamespaceGathering();
+			}
+
+			if (SignalToRefreshGUI)
+			{
+				RefreshNamespaceGatheringGUI();
+			}
+		}
+
+		private void RefreshNamespaceGatheringGUI()
+		{
+			Repaint();
+		}
+
+		private void NamespaceGatheringThreadMethod(object threadStopperObject)
+		{
+			try
+			{
+				var threadStopper = (ThreadStopper)threadStopperObject;
+
+				if (threadStopper.IsSignaledToStop)
+					return;
+
+				var path = Application.dataPath;
+				var sourceFiles = Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories);
+
+				if (sourceFiles != null)
+				{
+					// Look for namespaces in all files
+					for (int i = 0; i < sourceFiles.Length; i++)
 					{
-						foundNamespaceIndex = fileContent.IndexOf(NamespaceContentSearchString, foundNamespaceIndex);
-						if (foundNamespaceIndex < 0)
-							return;
+						// Read file content
+						var sourceFile = sourceFiles[i];
+						var fileContent = File.ReadAllText(sourceFile);
 
 						if (threadStopper.IsSignaledToStop)
 							return;
 
-						var previousChar = fileContent[foundNamespaceIndex - 1];
-						if (previousChar == '\n' || previousChar == '\r')
+						// Quickly search for namespace in file
+						var foundNamespaceIndex = 0;
+						while (true)
 						{
-							var namespaceStartIndex = foundNamespaceIndex + NamespaceContentSearchString.Length;
-							var lineEndIndex = fileContent.IndexOfNextLineEnding(namespaceStartIndex);
-							if (lineEndIndex < 0)
-								break; // There is something clearly wrong. Don't even try to handle this case.
-
-							// Found it!
-							var foundNamespace = fileContent.Substring(namespaceStartIndex, lineEndIndex - namespaceStartIndex);
+							foundNamespaceIndex = fileContent.IndexOf(NamespaceContentSearchString, foundNamespaceIndex);
+							if (foundNamespaceIndex < 0)
+								return;
 
 							if (threadStopper.IsSignaledToStop)
 								return;
 
-							AddToNamespaces(foundNamespace);
+							var previousChar = fileContent[foundNamespaceIndex - 1];
+							if (previousChar == '\n' || previousChar == '\r')
+							{
+								var namespaceStartIndex = foundNamespaceIndex + NamespaceContentSearchString.Length;
+								var lineEndIndex = fileContent.IndexOfNextLineEnding(namespaceStartIndex);
+								if (lineEndIndex < 0)
+									break; // There is something clearly wrong. Don't even try to handle this case.
+
+								// Found it!
+								var foundNamespace = fileContent.Substring(namespaceStartIndex, lineEndIndex - namespaceStartIndex);
+
+								if (threadStopper.IsSignaledToStop)
+									return;
+
+								AddToNamespaces(foundNamespace);
+								SignalToRefreshGUI = true;
+								break;
+							}
+
+							// Calculate progress
+							var progress = (float)i / sourceFiles.Length;
+
+							if (threadStopper.IsSignaledToStop)
+								return;
+							NamespaceGatheringProgress = progress;
 							SignalToRefreshGUI = true;
-							break;
 						}
-
-						// Calculate progress
-						var progress = (float)i / sourceFiles.Length;
-
-						if (threadStopper.IsSignaledToStop)
-							return;
-						NamespaceGatheringProgress = progress;
-						SignalToRefreshGUI = true;
 					}
 				}
+
+				if (threadStopper.IsSignaledToStop)
+					return;
+				SignalToFinalizeNamespaceGathering = true;
 			}
-
-			if (threadStopper.IsSignaledToStop)
-				return;
-			SignalToFinalizeNamespaceGathering = true;
-		}
-		catch
-		{
-		}
-	}
-
-	public void CancelNamespaceGathering(bool gracefullyEnded = false)
-	{
-		if (!IsGatheringNamespaces)
-			return;
-
-		if (gracefullyEnded)
-			NamespaceGatheringProgress = 1f;
-		else
-			NamespaceGatheringProgress = 0f;
-
-		NamespacesListToCheckAvailability = null;
-
-		NamespaceGatheringThreadStopper.IsSignaledToStop = true;
-		NamespaceGatheringThreadStopper = null;
-		// NamespaceGatheringThread.Join(); // No need to wait for join. Let the thread fade itself out.
-		NamespaceGatheringThread = null;
-
-		SignalToFinalizeNamespaceGathering = false;
-	}
-
-	private void FinalizeNamespaceGathering()
-	{
-		SignalToFinalizeNamespaceGathering = false;
-
-		// Remove detected unavailable namespaces
-		{
-			for (int i = 0; i < NamespacesListToCheckAvailability.Count; i++)
+			catch
 			{
-				var unavailableNamespace = NamespacesListToCheckAvailability[i];
-				Namespaces.Remove(unavailableNamespace);
 			}
+		}
+
+		public void CancelNamespaceGathering(bool gracefullyEnded = false)
+		{
+			if (!IsGatheringNamespaces)
+				return;
+
+			if (gracefullyEnded)
+				NamespaceGatheringProgress = 1f;
+			else
+				NamespaceGatheringProgress = 0f;
+
 			NamespacesListToCheckAvailability = null;
+
+			NamespaceGatheringThreadStopper.IsSignaledToStop = true;
+			NamespaceGatheringThreadStopper = null;
+			// NamespaceGatheringThread.Join(); // No need to wait for join. Let the thread fade itself out.
+			NamespaceGatheringThread = null;
+
+			SignalToFinalizeNamespaceGathering = false;
 		}
 
-		CancelNamespaceGathering(true);
-	}
-
-	public void SortNamespaces()
-	{
-		lock (NamespaceListLocker)
+		private void FinalizeNamespaceGathering()
 		{
-			if (Namespaces != null)
+			SignalToFinalizeNamespaceGathering = false;
+
+			// Remove detected unavailable namespaces
 			{
-				Namespaces.Sort();
-			}
-		}
-	}
-
-	public void AddToNamespaces(string value)
-	{
-		lock (NamespaceListLocker)
-		{
-			value = value.Trim();
-
-			NamespacesListToCheckAvailability.Remove(value);
-
-			if (Namespaces == null)
-			{
-				Namespaces = new List<string>(50);
+				for (int i = 0; i < NamespacesListToCheckAvailability.Count; i++)
+				{
+					var unavailableNamespace = NamespacesListToCheckAvailability[i];
+					Namespaces.Remove(unavailableNamespace);
+				}
+				NamespacesListToCheckAvailability = null;
 			}
 
-			if (Namespace.Contains(value))
-				return;
-
-			Namespaces.AddSorted(value);
+			CancelNamespaceGathering(true);
 		}
+
+		public void SortNamespaces()
+		{
+			lock (NamespaceListLocker)
+			{
+				if (Namespaces != null)
+				{
+					Namespaces.Sort();
+				}
+			}
+		}
+
+		public void AddToNamespaces(string value)
+		{
+			lock (NamespaceListLocker)
+			{
+				value = value.Trim();
+
+				NamespacesListToCheckAvailability.Remove(value);
+
+				if (Namespaces == null)
+				{
+					Namespaces = new List<string>(50);
+				}
+
+				if (Namespace.Contains(value))
+					return;
+
+				Namespaces.AddSorted(value);
+			}
+		}
+
+		#endregion
 	}
 
-	#endregion
 }
 
