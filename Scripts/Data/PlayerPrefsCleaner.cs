84bd17a10cfeb574e85b437a49b0233c3d3cd61d
Can Baycay <can.baycay@gmail.com>
2017-05-19 22:19:38 +0300

    Refactor: Namespace and move.

---
+++ Scripts/Data/PlayerPrefsCleaner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Extenity.DataToolbox
+{
+
+	public class PlayerPrefsCleaner : MonoBehaviour
+	{
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.F12) && (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)))
+			{
+				PlayerPrefs.DeleteAll();
+			}
+		}
+	}
+
+}
