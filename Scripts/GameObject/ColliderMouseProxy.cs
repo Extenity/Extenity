4b0eccb8b3cb02605035a13f81b60a2e854c86f4
Can Baycay <can.baycay@gmail.com>
2017-05-19 18:26:31 +0300

    Refactor: Namespace.

--- Scripts/GameObject/ColliderMouseProxy.cs
+++ Scripts/GameObject/ColliderMouseProxy.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
 
-public class ColliderMouseProxy : MonoBehaviour
+namespace Extenity.GameObjectToolbox
 {
-	public GameObject target;
 
-	private void OnMouseDown() { if (target != null) target.SendMessage("OnMouseDown", SendMessageOptions.DontRequireReceiver); }
-	private void OnMouseDrag() { if (target != null) target.SendMessage("OnMouseDrag", SendMessageOptions.DontRequireReceiver); }
-	private void OnMouseEnter() { if (target != null) target.SendMessage("OnMouseEnter", SendMessageOptions.DontRequireReceiver); }
-	private void OnMouseExit() { if (target != null) target.SendMessage("OnMouseExit", SendMessageOptions.DontRequireReceiver); }
-	private void OnMouseOver() { if (target != null) target.SendMessage("OnMouseOver", SendMessageOptions.DontRequireReceiver); }
-	private void OnMouseUp() { if (target != null) target.SendMessage("OnMouseUp", SendMessageOptions.DontRequireReceiver); }
-	private void OnMouseUpAsButton() { if (target != null) target.SendMessage("OnMouseUpAsButton", SendMessageOptions.DontRequireReceiver); }
+	public class ColliderMouseProxy : MonoBehaviour
+	{
+		public GameObject target;
+
+		private void OnMouseDown() { if (target != null) target.SendMessage("OnMouseDown", SendMessageOptions.DontRequireReceiver); }
+		private void OnMouseDrag() { if (target != null) target.SendMessage("OnMouseDrag", SendMessageOptions.DontRequireReceiver); }
+		private void OnMouseEnter() { if (target != null) target.SendMessage("OnMouseEnter", SendMessageOptions.DontRequireReceiver); }
+		private void OnMouseExit() { if (target != null) target.SendMessage("OnMouseExit", SendMessageOptions.DontRequireReceiver); }
+		private void OnMouseOver() { if (target != null) target.SendMessage("OnMouseOver", SendMessageOptions.DontRequireReceiver); }
+		private void OnMouseUp() { if (target != null) target.SendMessage("OnMouseUp", SendMessageOptions.DontRequireReceiver); }
+		private void OnMouseUpAsButton() { if (target != null) target.SendMessage("OnMouseUpAsButton", SendMessageOptions.DontRequireReceiver); }
+	}
+
 }
