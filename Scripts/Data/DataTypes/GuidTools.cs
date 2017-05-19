8c0072bd43d52b20e72c9bd4c047a2cc0bf1999f
Can Baycay <can.baycay@gmail.com>
2017-05-19 19:54:28 +0300

    Fix: GuidTools was using Extenity Guids instead of System Guids after namespace changes.

--- Scripts/Data/DataTypes/GuidTools.cs
+++ Scripts/Data/DataTypes/GuidTools.cs
@@ -5,6 +5,11 @@ namespace Extenity.DataToolbox
 
 	public static class GuidTools
 	{
+		public static System.Guid ToGuid(this byte[] bytes)
+		{
+			return new System.Guid(bytes);
+		}
+
 		#region Create Random
 
 		/// <summary>
@@ -12,14 +17,14 @@ namespace Extenity.DataToolbox
 		/// Note that this method of generating a guid is not reliable to be unique. The randomness properties of the guid is actually the same as the 
 		/// UnityEngine.Random (probably 32 bit seed).
 		/// </summary>
-		public static Guid NewPseudoGuid()
+		public static System.Guid NewPseudoGuid()
 		{
 			var data = new byte[16];
 			for (int i = 0; i < 16; i++)
 			{
 				data[i] = (byte)(Random.value * 256f);
 			}
-			return new Guid(data);
+			return new System.Guid(data);
 		}
 
 		#endregion
--- Scripts/Other/GuidExt.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public static class GuidExt
-{
-	public static Guid ToGuid(this byte[] bytes)
-	{
-		return new Guid(bytes);
-	}
-}
