b84da2d11e3c8fa40ba12a1de4f25256530c38b7
Can Baycay <can.baycay@gmail.com>
2017-07-12 14:08:13 +0300

    Trivial.

---
+++ Scripts/DLLBuilder/Editor/Compiler/ChangeDetector.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Extenity.DLLBuilder
+{
+
+	//public static struct FileInfo
+	//{
+	//	public 
+	//}
+
+	public static class ChangeDetector
+	{
+
+	}
+
+}
