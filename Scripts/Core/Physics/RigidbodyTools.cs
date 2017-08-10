65bf9ca5391572292c7891d7a09d2f5c3eb3cc05
Can Baycay <can.baycay@gmail.com>
2017-08-10 12:54:17 +0300

    Add: RigidbodyTools.HorizontalSpeed

--- Scripts/Core/Physics/RigidbodyTools.cs
+++ Scripts/Core/Physics/RigidbodyTools.cs
@@ -1,3 +1,4 @@
+using Extenity.MathToolbox;
 using UnityEngine;
 
 namespace Extenity.PhysicsToolbox
@@ -14,6 +15,12 @@ namespace Extenity.PhysicsToolbox
 		public static Vector3 LocalAngularVelocity(this Rigidbody rigidbody)
 		{
 			return Quaternion.Inverse(rigidbody.rotation) * rigidbody.angularVelocity;
+		}
+
+		public static float HorizontalSpeed(this Rigidbody rigidbody)
+		{
+			var velocity = rigidbody.velocity;
+			return Mathf.Sqrt(velocity.x * velocity.x + velocity.z * velocity.z);
 		}
 	}
 
