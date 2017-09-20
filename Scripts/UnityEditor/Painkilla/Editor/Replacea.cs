0ffe79427e9450f83182b1874ebfb5a31d2770f6
Can Baycay <can.baycay@gmail.com>
2017-09-20 17:39:14 +0300

    Add: Replacea tool.

--- Scripts/UnityEditor/Painkilla/Editor/Replacea.cs
+++ Scripts/UnityEditor/Painkilla/Editor/Replacea.cs
@@ -1,6 +1,7 @@
-// Work in progress.
-/*
-using System;
+using System.Collections.Generic;
+using System.Linq;
+using Extenity.DataToolbox;
+using Extenity.IMGUIToolbox.Editor;
 using Extenity.UnityEditorToolbox.Editor;
 using UnityEditor;
 using UnityEngine;
@@ -30,6 +31,8 @@ namespace Extenity.PainkillaTool.Editor
 			SetTitleAndIcon("Replacea", null);
 			minSize = MinimumWindowSize;
 
+			Selection.selectionChanged -= OnSelectionChanged;
+			Selection.selectionChanged += OnSelectionChanged;
 			//SceneView.onSceneGUIDelegate -= OnSceneGUI;
 			//SceneView.onSceneGUIDelegate += OnSceneGUI;
 		}
@@ -38,10 +41,11 @@ namespace Extenity.PainkillaTool.Editor
 
 		#region Deinitialization
 
-		//protected void OnDestroy()
-		//{
-		//	//SceneView.onSceneGUIDelegate -= OnSceneGUI;
-		//}
+		protected void OnDestroy()
+		{
+			Selection.selectionChanged -= OnSelectionChanged;
+			//SceneView.onSceneGUIDelegate -= OnSceneGUI;
+		}
 
 		#endregion
 
@@ -49,17 +53,55 @@ namespace Extenity.PainkillaTool.Editor
 
 		private readonly GUILayoutOption[] ReplaceButtonOptions = { GUILayout.Width(100f), GUILayout.Height(30f) };
 		private readonly GUIContent ReplaceButtonContent = new GUIContent("Replace", "Replaces all selected objects with the specified object.");
+		private readonly GUIContent ReplaceWithContent = new GUIContent("Replace With", "This object will be duplicated and replaced with selected objects.");
 
 		protected override void OnGUIDerived()
 		{
 			GUILayout.Space(8f);
 
-			GUILayout.BeginHorizontal();
-			if (GUILayout.Button(ReplaceButtonContent, "Button", ReplaceButtonOptions))
 			{
-				Replace();
+				var newReplaceWithObject = (Transform)EditorGUILayout.ObjectField(ReplaceWithContent, ReplaceWithObject, typeof(Transform), true);
+				if (ReplaceWithObject != newReplaceWithObject)
+				{
+					ReplaceWithObject = newReplaceWithObject;
+					OnSelectionChanged();
+				}
+
+				ReplaceRotations = EditorGUILayout.Toggle("Replace Rotations", ReplaceRotations);
+				ReplaceScales = EditorGUILayout.Toggle("Replace Scales", ReplaceScales);
+
+				EditorGUI.BeginDisabledGroup(FilteredSelection.IsNullOrEmpty() || ReplaceWithObject == null);
+				if (GUILayout.Button(ReplaceButtonContent, "Button", ReplaceButtonOptions))
+				{
+					Replace();
+				}
+				EditorGUI.EndDisabledGroup();
+			}
+
+			GUILayout.Space(20f);
+
+			GUILayout.BeginHorizontal();
+			EditorGUILayoutTools.DrawHeader("Selected Objects (" + FilteredSelection.SafeCount() + " of " + Selection.objects.SafeLength() + ")");
+			if (!FilteredSelection.IsNullOrEmpty())
+			{
+				if (GUILayout.Button("Select Filtered"))
+				{
+					EditorApplication.delayCall += SelectFilteredObjects;
+				}
 			}
 			GUILayout.EndHorizontal();
+			{
+				if (!FilteredSelection.IsNullOrEmpty())
+				{
+					//EditorGUI.BeginDisabledGroup(true);
+					for (var i = 0; i < FilteredSelection.Count; i++)
+					{
+						var selection = FilteredSelection[i];
+						EditorGUILayout.ObjectField("Object " + i.ToString(), selection, typeof(Transform), true);
+					}
+					//EditorGUI.EndDisabledGroup();
+				}
+			}
 
 			if (GUI.changed)
 			{
@@ -71,13 +113,61 @@ namespace Extenity.PainkillaTool.Editor
 
 		#region Replace
 
+		public Transform ReplaceWithObject;
+		public bool ReplaceRotations = false;
+		public bool ReplaceScales = false;
+
 		private void Replace()
 		{
-			throw new NotImplementedException();
+			if (FilteredSelection.IsNullOrEmpty())
+				return;
+			if (ReplaceWithObject == null)
+				return;
+
+			foreach (var selection in FilteredSelection)
+			{
+				var duplicate = Instantiate(ReplaceWithObject.gameObject, selection.parent).transform;
+				duplicate.localPosition = selection.localPosition;
+				if (!ReplaceRotations)
+					duplicate.localRotation = selection.localRotation;
+				if (!ReplaceScales)
+					duplicate.localScale = selection.localScale;
+				duplicate.gameObject.name = selection.gameObject.name;
+
+				Undo.DestroyObjectImmediate(selection.gameObject);
+				Undo.RegisterCreatedObjectUndo(duplicate.gameObject, "Replace Selected Objects");
+			}
+		}
+
+		#endregion
+
+		#region Selection
+
+		public List<Transform> FilteredSelection;
+
+		private void OnSelectionChanged()
+		{
+			FilteredSelection = GetSelection();
+			Repaint();
+		}
+
+		private List<Transform> GetSelection()
+		{
+			return Selection.GetFiltered<Transform>(SelectionMode.TopLevel)
+				.Where(transform => transform != null && transform != ReplaceWithObject)
+				.ToList();
+		}
+
+		private void SelectFilteredObjects()
+		{
+			if (FilteredSelection.IsNullOrEmpty())
+			{
+				Selection.activeObject = null;
+			}
+			Selection.objects = FilteredSelection.Select(transform => transform.gameObject).ToArray();
 		}
 
 		#endregion
 	}
 
 }
-*/
