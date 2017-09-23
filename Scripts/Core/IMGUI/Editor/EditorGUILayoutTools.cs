229448a2726872c62ae82e37630d4d95bd9b8c90
Can Baycay <can.baycay@gmail.com>
2017-09-23 23:26:20 +0300

    Add: EditorGUILayoutTools.UnwrapParam

--- Scripts/Core/IMGUI/Editor/EditorGUILayoutTools.cs
+++ Scripts/Core/IMGUI/Editor/EditorGUILayoutTools.cs
@@ -61,6 +61,36 @@ namespace Extenity.IMGUIToolbox.Editor
 		}
 
 		#endregion
+
+		#region UnwrapParam
+
+		// Copied from UnityEditor.dll/ModelImporterModelEditor.cs
+		private static GUIContent UnwrapParamContent_AngleDistortion = new GUIContent("Angle Error", "Measured in percents. Angle error measures deviation of UV angles from geometry angles. Area error measures deviation of UV triangles area from geometry triangles if they were uniformly scaled.");
+		private static GUIContent UnwrapParamContent_AreaDistortion = new GUIContent("Area Error", "Measured in percents. Angle error measures deviation of UV angles from geometry angles. Area error measures deviation of UV triangles area from geometry triangles if they were uniformly scaled.");
+		private static GUIContent UnwrapParamContent_HardAngle = new GUIContent("Hard Angle", "Angle between neighbor triangles that will generate seam.");
+		private static GUIContent UnwrapParamContent_PackMargin = new GUIContent("Pack Margin", "Measured in pixels, assuming mesh will cover an entire 1024x1024 lightmap.");
+
+		public static UnwrapParam UnwrapParam(UnwrapParam unwrapParam, bool showResetButton)
+		{
+			unwrapParam.hardAngle = EditorGUILayout.IntSlider(UnwrapParamContent_HardAngle, Mathf.RoundToInt(unwrapParam.hardAngle), 0, 180);
+			unwrapParam.packMargin = EditorGUILayout.IntSlider(UnwrapParamContent_PackMargin, Mathf.RoundToInt(unwrapParam.packMargin * 1024f), 1, 64) / 1024f;
+			unwrapParam.angleError = EditorGUILayout.IntSlider(UnwrapParamContent_AngleDistortion, Mathf.RoundToInt(unwrapParam.angleError * 100f), 1, 75) / 100f;
+			unwrapParam.areaError = EditorGUILayout.IntSlider(UnwrapParamContent_AreaDistortion, Mathf.RoundToInt(unwrapParam.areaError * 100f), 1, 75) / 100f;
+
+			if (showResetButton)
+			{
+				GUILayout.BeginHorizontal();
+				GUILayout.FlexibleSpace();
+				if (GUILayout.Button("Reset", GUILayout.ExpandWidth(false), GUILayout.Height(16f)))
+				{
+					UnityEditor.UnwrapParam.SetDefaults(out unwrapParam);
+				}
+				GUILayout.EndHorizontal();
+			}
+			return unwrapParam;
+		}
+
+		#endregion
 	}
 
 }
