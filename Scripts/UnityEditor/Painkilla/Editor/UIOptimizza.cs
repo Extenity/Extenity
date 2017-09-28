e47f9172dba72810ca2bb7b68f7bf666cc25a5d0
Can Baycay <can.baycay@gmail.com>
2017-09-28 13:50:28 +0300

    Add: UIOptimizza

---
+++ Scripts/UnityEditor/Painkilla/Editor/UIOptimizza.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Extenity.DataToolbox;
+using Extenity.GameObjectToolbox;
+using Extenity.IMGUIToolbox.Editor;
+using Extenity.TextureToolbox;
+using Extenity.UnityEditorToolbox.Editor;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Extenity.PainkillaTool.Editor
+{
+
+	public class UIOptimizza : ExtenityEditorWindowBase
+	{
+		#region Configuration
+
+		private static readonly Vector2 MinimumWindowSize = new Vector2(200f, 50f);
+
+		#endregion
+
+		#region Initialization
+
+		[MenuItem("Tools/Optimizza/UI Optimizza", false, 100)]
+		private static void ShowWindow()
+		{
+			var window = GetWindow<UIOptimizza>();
+			window.Show();
+		}
+
+		private void OnEnable()
+		{
+			SetTitleAndIcon("UI Optimizza", null);
+			minSize = MinimumWindowSize;
+
+			//ReplaceAsPrefabProperty = serializedObject.FindProperty("ReplaceAsPrefab");
+
+			//Selection.selectionChanged -= OnSelectionChanged;
+			//Selection.selectionChanged += OnSelectionChanged;
+			//SceneView.onSceneGUIDelegate -= OnSceneGUI;
+			//SceneView.onSceneGUIDelegate += OnSceneGUI;
+		}
+
+		#endregion
+
+		#region Deinitialization
+
+		protected void OnDestroy()
+		{
+			//Selection.selectionChanged -= OnSelectionChanged;
+			//SceneView.onSceneGUIDelegate -= OnSceneGUI;
+		}
+
+		#endregion
+
+		#region Serialized Properties
+
+		//private SerializedProperty ReplaceAsPrefabProperty;
+
+		#endregion
+
+		#region GUI - Window
+
+		private readonly GUILayoutOption[] FindButtonOptions = { GUILayout.Width(100f), GUILayout.Height(30f) };
+		private readonly GUIContent FindButtonContent = new GUIContent("Find", "Finds all UI elements which has a Raycast Target option in selected and children objects.");
+		private readonly GUILayoutOption[] ListEntryLeftButtonOptions = { GUILayout.Height(18f), GUILayout.Width(18f) };
+		private readonly GUILayoutOption[] ListEntryRightButtonOptions = { GUILayout.Height(18f) };
+		private readonly GUIContent ListEntryLeftButtonActiveContent = new GUIContent("", "Currently active");
+		private readonly GUIContent ListEntryLeftButtonInactiveContent = new GUIContent("", "Currently inactive");
+		private GUIStyle ListEntryStyleActive;
+		private GUIStyle ListEntryStyleInactive;
+
+		protected override void OnGUIDerived()
+		{
+			if (ListEntryStyleActive == null)
+			{
+				ListEntryStyleActive = new GUIStyle(EditorStyles.toggle);
+				ListEntryStyleActive.alignment = TextAnchor.MiddleLeft;
+				ListEntryStyleActive.normal.background = TextureTools.CreateSimpleTexture(4, 4, Color.blue);
+				ListEntryStyleInactive = new GUIStyle(ListEntryStyleActive);
+				ListEntryStyleInactive.normal.background = TextureTools.CreateSimpleTexture(4, 4, Color.grey);
+			}
+
+			GUILayout.Space(8f);
+
+			if (GUILayout.Button(FindButtonContent, FindButtonOptions))
+			{
+				FindInSelection();
+			}
+
+			GUILayout.Space(20f);
+
+			var filteredCount = FilteredSelection.SafeCount();
+			EditorGUILayoutTools.DrawHeader("Selected Graphics (" + filteredCount + ")");
+			if (!FilteredSelection.IsNullOrEmpty())
+			{
+				ScrollPosition = EditorGUILayout.BeginScrollView(ScrollPosition, false, false);
+				for (var i = 0; i < FilteredSelection.Count; i++)
+				{
+					var selection = FilteredSelection[i];
+					var currentlyEnabled = selection.Graphic.raycastTarget;
+					var style = currentlyEnabled ? ListEntryStyleActive : ListEntryStyleInactive;
+					var leftButtonContent = currentlyEnabled ? ListEntryLeftButtonActiveContent : ListEntryLeftButtonInactiveContent;
+					EditorGUILayout.BeginHorizontal();
+					var toggle = GUILayout.Toggle(currentlyEnabled, leftButtonContent, style, ListEntryLeftButtonOptions);
+					if (toggle != currentlyEnabled)
+					{
+						selection.Graphic.raycastTarget = !currentlyEnabled;
+						Repaint();
+					}
+					if (GUILayout.Button(selection.GUIContent, style, ListEntryRightButtonOptions))
+					{
+						Selection.activeGameObject = selection.Graphic.gameObject;
+					}
+					EditorGUILayout.EndHorizontal();
+				}
+				EditorGUILayout.EndScrollView();
+			}
+		}
+
+		#endregion
+
+		#region Selection
+
+		public struct SelectionEntry
+		{
+			public string Name;
+			public Graphic Graphic;
+			public GUIContent GUIContent;
+
+			public SelectionEntry(string name, Graphic graphic)
+			{
+				Name = name;
+				Graphic = graphic;
+				GUIContent = new GUIContent(name);
+			}
+		}
+
+		public List<SelectionEntry> FilteredSelection;
+
+		private void FindInSelection()
+		{
+			var selectedObjects = Selection.GetFiltered<Graphic>(SelectionMode.Deep);
+
+			if (FilteredSelection != null)
+				FilteredSelection.Clear();
+			else
+				FilteredSelection = new List<SelectionEntry>(100);
+
+			foreach (var selectedObject in selectedObjects)
+			{
+				FilteredSelection.Add(new SelectionEntry(selectedObject.gameObject.FullName(), selectedObject));
+			}
+
+			Repaint();
+		}
+
+		private void SelectFilteredObjects()
+		{
+			if (FilteredSelection.IsNullOrEmpty())
+			{
+				Selection.activeObject = null;
+			}
+			Selection.objects = FilteredSelection.Select(selected => selected.Graphic.gameObject).ToArray();
+		}
+
+		#endregion
+	}
+
+}
