2394dfbcfe44c0d87ca9d82ac7c863c328945b57
Can Baycay <can.baycay@gmail.com>
2018-01-17 17:04:55 +0300

    Add: NavigationTools

---
+++ Scripts/Core/Navigation/NavigationTools.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Extenity.NavigationToolbox
+{
+
+	public static class NavigationTools
+	{
+		public static void DrawPath(this NavMeshPath path, LineRenderer lineRenderer)
+		{
+			if (!lineRenderer)
+				return;
+
+			if (path.corners.Length < 2)
+			{
+				lineRenderer.positionCount = 0;
+			}
+			else
+			{
+				lineRenderer.positionCount = path.corners.Length;
+				lineRenderer.SetPositions(path.corners);
+			}
+		}
+
+		public static void DrawDebugPath(this NavMeshPath path, Color color, float duration = 0f, bool depthTest = false)
+		{
+			var corners = path.corners;
+			for (int i = 0; i < corners.Length - 1; i++)
+				Debug.DrawLine(corners[i], corners[i + 1], color, duration, depthTest);
+		}
+	}
+
+}
