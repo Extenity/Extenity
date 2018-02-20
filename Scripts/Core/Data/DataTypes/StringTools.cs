315c76645bd4be83adf7ec2e277166f092654cc1
Can Baycay <can.baycay@gmail.com>
2018-02-20 21:03:41 +0300

    Add: StringTools.TrimAll

--- Scripts/Core/Data/DataTypes/StringTools.cs
+++ Scripts/Core/Data/DataTypes/StringTools.cs
@@ -826,6 +826,60 @@ namespace Extenity.DataToolbox
 
 		#endregion
 
+		#region Whitespaces
+
+		/// <summary>
+		/// Source: https://www.codeproject.com/Articles/1014073/Fastest-method-to-remove-all-whitespace-from-Strin
+		/// </summary>
+		public static string TrimAll(this string value)
+		{
+			if (value == null)
+				return null;
+			var length = value.Length;
+			var copy = value.ToCharArray();
+
+			int iDestination = 0;
+			for (int iSource = 0; iSource < length; iSource++)
+			{
+				var ch = copy[iSource];
+				switch (ch)
+				{
+					case '\u0020':
+					case '\u00A0':
+					case '\u1680':
+					case '\u2000':
+					case '\u2001':
+					case '\u2002':
+					case '\u2003':
+					case '\u2004':
+					case '\u2005':
+					case '\u2006':
+					case '\u2007':
+					case '\u2008':
+					case '\u2009':
+					case '\u200A':
+					case '\u202F':
+					case '\u205F':
+					case '\u3000':
+					case '\u2028':
+					case '\u2029':
+					case '\u0009':
+					case '\u000A':
+					case '\u000B':
+					case '\u000C':
+					case '\u000D':
+					case '\u0085':
+						continue;
+					default:
+						copy[iDestination++] = ch;
+						break;
+				}
+			}
+			return new string(copy, 0, iDestination);
+		}
+
+		#endregion
+
 		#region Serialization
 
 		public static string Serialize<T>(this T array, char separator = ',', int capacity = 0) where T : ICollection
