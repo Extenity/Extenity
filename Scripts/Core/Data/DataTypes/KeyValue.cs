0e6e092aea2afce270cf5ad701a6d5af56bfd6e0
Can Baycay <can.baycay@gmail.com>
2018-02-24 16:47:56 +0300

    Add: KeyValue

---
+++ Scripts/Core/Data/DataTypes/KeyValue.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Extenity.DataToolbox
+{
+
+	[Serializable]
+	public struct KeyValue<TKey, TValue>
+	{
+		#region Data
+
+		public TKey Key;
+		public TValue Value;
+
+		#endregion
+
+		#region Initialization
+
+		public KeyValue(TKey key, TValue value)
+		{
+			Key = key;
+			Value = value;
+		}
+
+		#endregion
+
+		#region Conversions
+
+		public override string ToString()
+		{
+			return Key.ToString() + ": " + Value.ToString();
+		}
+
+		#endregion
+	}
+
+}
