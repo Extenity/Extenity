6e3877e1ef022cc580cace643007613ed55998f2
Can Baycay <can.baycay@gmail.com>
2018-03-08 14:58:25 +0300

    Add: RigidbodySleepyStart

---
+++ Scripts/Core/Physics/Editor/RigidbodySleepyStartInspector.cs
@@ -0,0 +1,37 @@
+using Extenity.UnityEditorToolbox.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Extenity.PhysicsToolbox
+{
+
+	[CustomEditor(typeof(RigidbodySleepyStart))]
+	public class RigidbodySleepyStartInspector : ExtenityEditorBase<RigidbodySleepyStart>
+	{
+		protected override void OnEnableDerived()
+		{
+		}
+
+		protected override void OnDisableDerived()
+		{
+		}
+
+		protected override void OnBeforeDefaultInspectorGUI()
+		{
+			GUILayout.Space(20f);
+
+			if (GUILayout.Button("Get All Rigidbodies In Loaded Scenes", BigButtonHeight))
+			{
+				Undo.RecordObject(Me, "Gather all rigidbodies");
+				Me.GatherAllRigidbodiesInLoadedScenes(true);
+			}
+
+			GUILayout.Space(20f);
+		}
+
+		protected override void OnAfterDefaultInspectorGUI()
+		{
+		}
+	}
+
+}
---
+++ Scripts/Core/Physics/RigidbodySleepyStart.cs
@@ -0,0 +1,74 @@
+using Extenity.GameObjectToolbox;
+using UnityEngine;
+
+namespace Extenity.PhysicsToolbox
+{
+
+	public class RigidbodySleepyStart : MonoBehaviour
+	{
+		#region Initialization
+
+		protected void Start()
+		{
+			if (AutoSleepOnStart)
+			{
+				SleepAllRigidbodies();
+			}
+			if (AutoDestroyOnStart)
+			{
+				Destroy(gameObject);
+			}
+		}
+
+		#endregion
+
+		#region Gather
+
+		public GameObject[] FilterOutGameObjects;
+
+		public void GatherAllRigidbodiesInLoadedScenes(bool logFilteredOutRigidbodies)
+		{
+			var rigidbodies = GameObjectTools.FindObjectsOfTypeAllInLoadedScenes<Rigidbody>();
+			for (int i = 0; i < rigidbodies.Count; i++)
+			{
+				for (int iFilter = 0; iFilter < FilterOutGameObjects.Length; iFilter++)
+				{
+					if (rigidbodies[i].transform.IsChildOf(FilterOutGameObjects[iFilter].transform))
+					{
+						if (logFilteredOutRigidbodies)
+						{
+							Debug.Log("Filtered: " + rigidbodies[i].gameObject, rigidbodies[i].gameObject);
+						}
+						rigidbodies.RemoveAt(i);
+						i--;
+					}
+				}
+			}
+			Rigidbodies = rigidbodies.ToArray();
+		}
+
+		#endregion
+
+		#region Sleep
+
+		public bool AutoSleepOnStart = true;
+		public bool AutoDestroyOnStart = true;
+
+		public void SleepAllRigidbodies()
+		{
+			for (var i = 0; i < Rigidbodies.Length; i++)
+			{
+				Rigidbodies[i].Sleep();
+			}
+		}
+
+		#endregion
+
+		#region Rigidbodies
+
+		public Rigidbody[] Rigidbodies;
+
+		#endregion
+	}
+
+}
