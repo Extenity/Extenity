e7ddd8eec44a6b056c55302125934876cae475bd
Can Baycay <can.baycay@gmail.com>
2018-03-15 04:12:45 +0300

    Add: Material.GetAllTextures

---
+++ Scripts/Core/Rendering/Editor/EditorMaterialTools.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Extenity.RenderingToolbox.Editor
+{
+
+	public static class EditorMaterialTools
+	{
+		#region Texture
+
+		public static List<Texture> GetAllTextures(this Material material)
+		{
+			var result = new List<Texture>();
+			var shader = material.shader;
+			var propertyCount = ShaderUtil.GetPropertyCount(shader);
+			for (int i = 0; i < propertyCount; i++)
+			{
+				if (ShaderUtil.GetPropertyType(shader, i) == ShaderUtil.ShaderPropertyType.TexEnv)
+				{
+					var propertyName = ShaderUtil.GetPropertyName(shader, i);
+					var texture = material.GetTexture(propertyName);
+					result.Add(texture);
+				}
+			}
+			return result;
+		}
+
+		#endregion
+
+	}
+
+}
