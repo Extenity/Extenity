1e19a002018d7329722eb787ab74b03068d0a3c1
Can Baycay <can.baycay@gmail.com>
2018-04-01 22:07:46 +0300

    Change: RunningHotMean split into two classes with double and float support.

--- Scripts/Core/Math/RunningHotMean.cs
+++ Scripts/Core/Math/RunningHotMean.cs
@@ -3,9 +3,12 @@
 namespace Extenity.MathToolbox
 {
 
-	public class RunningHotMean
+	public class RunningHotMeanDouble
 	{
-		private CircularArray<double> Values;
+		/// <summary>
+		/// CAUTION! Use it as readonly, do not modify. Use 'Push' and 'Clear' instead.
+		/// </summary>
+		public CircularArray<double> Values;
 		public int ValueCount { get { return Values.Count; } }
 
 		private bool IsInvalidated;
@@ -33,7 +36,7 @@ namespace Extenity.MathToolbox
 			return total / valueCount;
 		}
 
-		public RunningHotMean(int size)
+		public RunningHotMeanDouble(int size)
 		{
 			Values = new CircularArray<double>(size);
 			IsInvalidated = false;
@@ -48,6 +51,60 @@ namespace Extenity.MathToolbox
 		}
 
 		public void Push(double value)
+		{
+			IsInvalidated = true;
+			Values.Add(value);
+		}
+	}
+
+	public class RunningHotMeanFloat
+	{
+		/// <summary>
+		/// CAUTION! Use it as readonly, do not modify. Use 'Push' and 'Clear' instead.
+		/// </summary>
+		public CircularArray<float> Values;
+		public int ValueCount { get { return Values.Count; } }
+
+		private bool IsInvalidated;
+
+		private float _Mean;
+		public float Mean
+		{
+			get
+			{
+				if (IsInvalidated)
+				{
+					_Mean = CalculateMean();
+				}
+				return _Mean;
+			}
+		}
+
+		private float CalculateMean()
+		{
+			var valueCount = Values.Count;
+			if (valueCount == 0)
+				return 0;
+			float total = 0;
+			Values.ForEach(item => total += item);
+			return total / valueCount;
+		}
+
+		public RunningHotMeanFloat(int size)
+		{
+			Values = new CircularArray<float>(size);
+			IsInvalidated = false;
+			_Mean = 0;
+		}
+
+		public void Clear()
+		{
+			Values.Clear();
+			IsInvalidated = false;
+			_Mean = 0;
+		}
+
+		public void Push(float value)
 		{
 			IsInvalidated = true;
 			Values.Add(value);
