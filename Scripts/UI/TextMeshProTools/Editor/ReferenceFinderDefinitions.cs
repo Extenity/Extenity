7b523282b4eadf386f629280a037c4b1189c37bc
Can Baycay <can.baycay@gmail.com>
2018-07-03 01:27:07 +0300

    TextMesh Pro updated to version 1.2.3 via Package Manager

--- Scripts/UI/TextMeshProTools/Editor/ReferenceFinderDefinitions.cs
+++ Scripts/UI/TextMeshProTools/Editor/ReferenceFinderDefinitions.cs
@@ -11,12 +11,11 @@ namespace TMPro.Extensions.Editor
 		{
 			ReflectionTools.KnownTypesOfGameObjectReferenceFinder.Add(typeof(FaceInfo));
 			ReflectionTools.KnownTypesOfGameObjectReferenceFinder.Add(typeof(TMP_TextInfo));
-			ReflectionTools.KnownTypesOfGameObjectReferenceFinder.Add(typeof(TMP_FontAsset));
+			//ReflectionTools.KnownTypesOfGameObjectReferenceFinder.Add(typeof(TMP_FontAsset)); This gives an error related to ScriptableObject not being found in referenced DLLs at compile time.
 			ReflectionTools.KnownTypesOfGameObjectReferenceFinder.Add(typeof(TMP_FontWeights));
 			ReflectionTools.KnownTypesOfGameObjectReferenceFinder.Add(typeof(TMP_Glyph));
 			ReflectionTools.KnownTypesOfGameObjectReferenceFinder.Add(typeof(KerningTable));
 			ReflectionTools.KnownTypesOfGameObjectReferenceFinder.Add(typeof(KerningPair));
-			ReflectionTools.KnownTypesOfGameObjectReferenceFinder.Add(typeof(FontCreationSetting));
 			ReflectionTools.KnownTypesOfGameObjectReferenceFinder.Add(typeof(VertexGradient));
 		}
 	}
