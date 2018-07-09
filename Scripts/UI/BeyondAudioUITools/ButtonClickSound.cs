e4a04bffd75e0cb7ad12f2db7eba549f6d6417a3
Can Baycay <can.baycay@gmail.com>
2018-07-09 13:14:32 +0300

    Fix: Button click sound was not working with objects created while loading the scene.

--- Scripts/UI/BeyondAudioUITools/ButtonClickSound.cs
+++ Scripts/UI/BeyondAudioUITools/ButtonClickSound.cs
@@ -1,4 +1,4 @@
-using Extenity.FlowToolbox;
+using Extenity.DataToolbox;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,8 +11,6 @@ namespace Extenity.BeyondAudio.UI
 		public Toggle Toggle;
 		public string EventName = "ButtonClick";
 
-		public static bool IsLoggingEnabled;
-
 		public static bool IsTypeSupported<T>()
 		{
 			return
@@ -22,27 +20,37 @@ namespace Extenity.BeyondAudio.UI
 
 		protected void OnEnable()
 		{
-			// Allow everything to be initialized first. The user would not likely press the button in 100 ms.
-			this.FastInvoke(OnEnableDelayed, 0.1);
+			// Wow! RegisterEvents does not get called on some buttons for some reason. Don't know why, and don't care for now. It's not about FastInvoke because Invoke does not work too.
+			//// Allow everything to be initialized first. The user would not likely press the button in 100 ms.
+			//this.FastInvoke("RegisterEvents", 0.1f);
+
+			RegisterEvents();
 		}
 
-		private void OnEnableDelayed()
+		protected void OnDisable()
+		{
+			DeregisterEvents();
+		}
+
+		private void RegisterEvents()
 		{
 			if (Button)
 			{
 				if (IsLoggingEnabled)
 					Debug.Log($"Registering '{typeof(Button).Name}' click sound for '{gameObject.name}'.", this);
+				Button.onClick.RemoveListener(OnClick);
 				Button.onClick.AddListener(OnClick);
 			}
 			else if (Toggle)
 			{
 				if (IsLoggingEnabled)
 					Debug.Log($"Registering '{typeof(Toggle).Name}' click sound for '{gameObject.name}'.", this);
+				Toggle.onValueChanged.RemoveListener(OnValueChanged);
 				Toggle.onValueChanged.AddListener(OnValueChanged);
 			}
 		}
 
-		protected void OnDisable()
+		private void DeregisterEvents()
 		{
 			if (Button)
 			{
@@ -82,6 +90,43 @@ namespace Extenity.BeyondAudio.UI
 				Toggle = GetComponent<Toggle>();
 			}
 		}
+
+		#region Log
+
+		private const string LoggingPrefKey = "EnableButtonClickSoundLogging";
+
+		private static bool _IsLoggingInitialized;
+
+		private static bool _IsLoggingEnabled;
+		public static bool IsLoggingEnabled
+		{
+			get
+			{
+				if (!_IsLoggingInitialized)
+				{
+					_IsLoggingEnabled = PlayerPrefsTools.GetBool(LoggingPrefKey, false);
+					_IsLoggingInitialized = true;
+				}
+				return _IsLoggingEnabled;
+			}
+			set
+			{
+				_IsLoggingEnabled = value;
+				_IsLoggingInitialized = true;
+				if (value)
+				{
+					PlayerPrefsTools.SetBool(LoggingPrefKey, value);
+				}
+				else
+				{
+					// This is a debugging tool. No need to keep the registry key around.
+					PlayerPrefs.DeleteKey(LoggingPrefKey);
+				}
+				PlayerPrefs.Save();
+			}
+		}
+
+		#endregion
 	}
 
 }
