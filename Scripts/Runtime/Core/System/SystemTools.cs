3c6888e56e0028947a92b800fe2eb7f3cb38a35c
Can Baycay <can.baycay@gmail.com>
2018-08-08 16:20:49 +0300

    Add: Action.InvokeSafe improvements.

--- Scripts/Runtime/Core/System/SystemTools.cs
+++ Scripts/Runtime/Core/System/SystemTools.cs
@@ -8,94 +8,358 @@ namespace Extenity.SystemToolbox
 	{
 		#region Action InvokeSafe
 
-		public static void InvokeSafe(this Action action)
+		public static bool InvokeSafe(this Action action)
 		{
 			if (action != null)
 			{
 				try // Safety belt in case something goes wrong in callback.
 				{
 					action();
+					return true;
 				}
 				catch (Exception e)
 				{
 					Debug.LogException(e);
 				}
 			}
+			return false;
 		}
 
-		public static void InvokeSafe<T1>(this Action<T1> action, T1 arg1)
+		public static bool InvokeSafe<T1>(this Action<T1> action, T1 arg1)
 		{
 			if (action != null)
 			{
 				try // Safety belt in case something goes wrong in callback.
 				{
 					action(arg1);
+					return true;
 				}
 				catch (Exception e)
 				{
 					Debug.LogException(e);
 				}
 			}
+			return false;
 		}
 
-		public static void InvokeSafe<T1, T2>(this Action<T1, T2> action, T1 arg1, T2 arg2)
+		public static bool InvokeSafe<T1, T2>(this Action<T1, T2> action, T1 arg1, T2 arg2)
 		{
 			if (action != null)
 			{
 				try // Safety belt in case something goes wrong in callback.
 				{
 					action(arg1, arg2);
+					return true;
 				}
 				catch (Exception e)
 				{
 					Debug.LogException(e);
 				}
 			}
+			return false;
 		}
 
-		public static void InvokeSafe<T1, T2, T3>(this Action<T1, T2, T3> action, T1 arg1, T2 arg2, T3 arg3)
+		public static bool InvokeSafe<T1, T2, T3>(this Action<T1, T2, T3> action, T1 arg1, T2 arg2, T3 arg3)
 		{
 			if (action != null)
 			{
 				try // Safety belt in case something goes wrong in callback.
 				{
 					action(arg1, arg2, arg3);
+					return true;
 				}
 				catch (Exception e)
 				{
 					Debug.LogException(e);
 				}
 			}
+			return false;
 		}
 
-		public static void InvokeSafe<T1, T2, T3, T4>(this Action<T1, T2, T3, T4> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+		public static bool InvokeSafe<T1, T2, T3, T4>(this Action<T1, T2, T3, T4> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
 		{
 			if (action != null)
 			{
 				try // Safety belt in case something goes wrong in callback.
 				{
 					action(arg1, arg2, arg3, arg4);
+					return true;
 				}
 				catch (Exception e)
 				{
 					Debug.LogException(e);
 				}
 			}
+			return false;
 		}
 
-		public static void InvokeSafe<T1, T2, T3, T4, T5>(this Action<T1, T2, T3, T4, T5> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+		public static bool InvokeSafe<T1, T2, T3, T4, T5>(this Action<T1, T2, T3, T4, T5> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
 		{
 			if (action != null)
 			{
 				try // Safety belt in case something goes wrong in callback.
 				{
 					action(arg1, arg2, arg3, arg4, arg5);
+					return true;
 				}
 				catch (Exception e)
 				{
 					Debug.LogException(e);
 				}
 			}
+			return false;
+		}
+
+		public static bool InvokeSafe<T1, T2, T3, T4, T5, T6>(this Action<T1, T2, T3, T4, T5, T6> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6)
+		{
+			if (action != null)
+			{
+				try // Safety belt in case something goes wrong in callback.
+				{
+					action(arg1, arg2, arg3, arg4, arg5, arg6);
+					return true;
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+			return false;
+		}
+
+		public static bool InvokeSafe<T1, T2, T3, T4, T5, T6, T7>(this Action<T1, T2, T3, T4, T5, T6, T7> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7)
+		{
+			if (action != null)
+			{
+				try // Safety belt in case something goes wrong in callback.
+				{
+					action(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+					return true;
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+			return false;
+		}
+
+		public static bool InvokeSafe<T1, T2, T3, T4, T5, T6, T7, T8>(this Action<T1, T2, T3, T4, T5, T6, T7, T8> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8)
+		{
+			if (action != null)
+			{
+				try // Safety belt in case something goes wrong in callback.
+				{
+					action(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
+					return true;
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+			return false;
+		}
+
+		public static bool InvokeSafe<T1, T2, T3, T4, T5, T6, T7, T8, T9>(this Action<T1, T2, T3, T4, T5, T6, T7, T8, T9> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9)
+		{
+			if (action != null)
+			{
+				try // Safety belt in case something goes wrong in callback.
+				{
+					action(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
+					return true;
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+			return false;
+		}
+
+		#endregion
+
+		#region Func InvokeSafe
+
+		public static bool InvokeSafe<TResult>(this Func<TResult> action, out TResult result)
+		{
+			if (action != null)
+			{
+				try // Safety belt in case something goes wrong in callback.
+				{
+					result = action();
+					return true;
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+			result = default(TResult);
+			return false;
+		}
+		
+		public static bool InvokeSafe<T1, TResult>(this Func<T1, TResult> action, T1 arg1, out TResult result)
+		{
+			if (action != null)
+			{
+				try // Safety belt in case something goes wrong in callback.
+				{
+					result = action(arg1);
+					return true;
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+			result = default(TResult);
+			return false;
+		}
+		
+		public static bool InvokeSafe<T1, T2, TResult>(this Func<T1, T2, TResult> action, T1 arg1, T2 arg2, out TResult result)
+		{
+			if (action != null)
+			{
+				try // Safety belt in case something goes wrong in callback.
+				{
+					result = action(arg1, arg2);
+					return true;
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+			result = default(TResult);
+			return false;
+		}
+		
+		public static bool InvokeSafe<T1, T2, T3, TResult>(this Func<T1, T2, T3, TResult> action, T1 arg1, T2 arg2, T3 arg3, out TResult result)
+		{
+			if (action != null)
+			{
+				try // Safety belt in case something goes wrong in callback.
+				{
+					result = action(arg1, arg2, arg3);
+					return true;
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+			result = default(TResult);
+			return false;
+		}
+		
+		public static bool InvokeSafe<T1, T2, T3, T4, TResult>(this Func<T1, T2, T3, T4, TResult> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4, out TResult result)
+		{
+			if (action != null)
+			{
+				try // Safety belt in case something goes wrong in callback.
+				{
+					result = action(arg1, arg2, arg3, arg4);
+					return true;
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+			result = default(TResult);
+			return false;
+		}
+		
+		public static bool InvokeSafe<T1, T2, T3, T4, T5, TResult>(this Func<T1, T2, T3, T4, T5, TResult> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, out TResult result)
+		{
+			if (action != null)
+			{
+				try // Safety belt in case something goes wrong in callback.
+				{
+					result = action(arg1, arg2, arg3, arg4, arg5);
+					return true;
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+			result = default(TResult);
+			return false;
+		}
+		
+		public static bool InvokeSafe<T1, T2, T3, T4, T5, T6, TResult>(this Func<T1, T2, T3, T4, T5, T6, TResult> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, out TResult result)
+		{
+			if (action != null)
+			{
+				try // Safety belt in case something goes wrong in callback.
+				{
+					result = action(arg1, arg2, arg3, arg4, arg5, arg6);
+					return true;
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+			result = default(TResult);
+			return false;
+		}
+		
+		public static bool InvokeSafe<T1, T2, T3, T4, T5, T6, T7, TResult>(this Func<T1, T2, T3, T4, T5, T6, T7, TResult> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, out TResult result)
+		{
+			if (action != null)
+			{
+				try // Safety belt in case something goes wrong in callback.
+				{
+					result = action(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+					return true;
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+			result = default(TResult);
+			return false;
+		}
+		
+		public static bool InvokeSafe<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(this Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, out TResult result)
+		{
+			if (action != null)
+			{
+				try // Safety belt in case something goes wrong in callback.
+				{
+					result = action(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
+					return true;
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+			result = default(TResult);
+			return false;
+		}
+		
+		public static bool InvokeSafe<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, out TResult result)
+		{
+			if (action != null)
+			{
+				try // Safety belt in case something goes wrong in callback.
+				{
+					result = action(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
+					return true;
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+			result = default(TResult);
+			return false;
 		}
 
 		#endregion
