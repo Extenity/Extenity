462dc6ee147e72a67a22e9a15a2c22f2479ff443
Can Baycay <can.baycay@gmail.com>
2018-08-14 14:44:56 +0300

    Add: BoolEditorOption

---
+++ Scripts/Editor/Core/UnityEditor/BoolEditorOption.cs
@@ -0,0 +1,44 @@
+using UnityEditor;
+
+namespace Extenity.UnityEditorToolbox.Editor
+{
+
+	public class BoolEditorOption
+	{
+		public BoolEditorOption(string prefsKey, bool defaultValue)
+		{
+			PrefsKey = prefsKey;
+			_Value = defaultValue;
+		}
+
+		public readonly string PrefsKey;
+
+		private bool _IsInitialized;
+		private bool _Value;
+		public bool Value
+		{
+			get
+			{
+				if (!_IsInitialized)
+				{
+					_IsInitialized = true;
+					_Value = EditorPrefs.GetBool(PrefsKey, _Value);
+				}
+				return _Value;
+			}
+			set
+			{
+				_IsInitialized = true;
+				_Value = value;
+				EditorPrefs.SetBool(PrefsKey, value);
+			}
+		}
+
+		public bool Toggle()
+		{
+			Value = !Value;
+			return Value;
+		}
+	}
+
+}
