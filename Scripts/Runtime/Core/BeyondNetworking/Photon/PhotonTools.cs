4d2a4e566ea87e747a766186748bde4a75371ebc
Can Baycay <can.baycay@gmail.com>
2018-09-19 14:52:49 +0300

    Add: Photon serialization helpers.

--- Scripts/Runtime/Core/BeyondNetworking/Photon/PhotonTools.cs
+++ Scripts/Runtime/Core/BeyondNetworking/Photon/PhotonTools.cs
@@ -1,3 +1,4 @@
+using ExitGames.Client.Photon;
 using Extenity.DataToolbox;
 using Photon.Realtime;
 
@@ -36,6 +37,123 @@ namespace BeyondNetworking
 			return string.IsNullOrEmpty(player.NickName)
 				? player.ActorNumber.ToString()
 				: player.NickName;
+		}
+
+		#endregion
+
+		#region Serialization
+
+		private static readonly byte[] _Buffer = new byte[4];
+
+		public static int Serialize(this StreamBuffer outStream, int value)
+		{
+			lock (_Buffer)
+			{
+				var index = 0;
+				Protocol.Serialize(value, _Buffer, ref index);
+				outStream.Write(_Buffer, 0, index);
+				return index;
+			}
+		}
+
+		public static int Serialize(this StreamBuffer outStream, short value)
+		{
+			lock (_Buffer)
+			{
+				var index = 0;
+				Protocol.Serialize(value, _Buffer, ref index);
+				outStream.Write(_Buffer, 0, index);
+				return index;
+			}
+		}
+
+		public static int Serialize(this StreamBuffer outStream, float value)
+		{
+			lock (_Buffer)
+			{
+				var index = 0;
+				Protocol.Serialize(value, _Buffer, ref index);
+				outStream.Write(_Buffer, 0, index);
+				return index;
+			}
+		}
+
+		public static int SerializeAsShort(this StreamBuffer outStream, int value)
+		{
+			lock (_Buffer)
+			{
+				var index = 0;
+				var valueShort = (short)value;
+				Protocol.Serialize(valueShort, _Buffer, ref index);
+				outStream.Write(_Buffer, 0, index);
+				return index;
+			}
+		}
+
+		public static bool Deserialize(this StreamBuffer inStream, out int value)
+		{
+			lock (_Buffer)
+			{
+				var index = 0;
+				var readSize = inStream.Read(_Buffer, 0, sizeof(int));
+				if (readSize != sizeof(int))
+				{
+					value = int.MinValue;
+					return false;
+				}
+				Protocol.Deserialize(out value, _Buffer, ref index);
+				return true;
+			}
+		}
+
+		public static bool Deserialize(this StreamBuffer inStream, out short value)
+		{
+			lock (_Buffer)
+			{
+				var index = 0;
+				var readSize = inStream.Read(_Buffer, 0, sizeof(short));
+				if (readSize != sizeof(short))
+				{
+					value = short.MinValue;
+					return false;
+				}
+				Protocol.Deserialize(out value, _Buffer, ref index);
+				return true;
+			}
+		}
+
+		public static bool Deserialize(this StreamBuffer inStream, out float value)
+		{
+			lock (_Buffer)
+			{
+				var index = 0;
+				var readSize = inStream.Read(_Buffer, 0, sizeof(float));
+				if (readSize != sizeof(float))
+				{
+					value = float.NaN;
+					return false;
+				}
+				Protocol.Deserialize(out value, _Buffer, ref index);
+				return true;
+			}
+		}
+
+		public static bool DeserializeAsShort(this StreamBuffer inStream, out int value)
+		{
+			lock (_Buffer)
+			{
+				var index = 0;
+				var readSize = inStream.Read(_Buffer, 0, sizeof(short));
+				if (readSize != sizeof(short))
+				{
+					value = int.MinValue;
+					return false;
+				}
+				short valueShort;
+				Protocol.Deserialize(out valueShort, _Buffer, ref index);
+				value = valueShort;
+				return true;
+			}
 		}
 
 		#endregion
