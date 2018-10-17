7b7926b23198472caab1166344e1818fe7362503
Can Baycay <can.baycay@gmail.com>
2018-10-17 18:26:17 +0300

    Add: UIFadeOnClick

---
+++ Scripts/Runtime/UI/Animation/Fader/UIFadeOnClick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Extenity.UIToolbox
+{
+
+	public class UIFadeOnClick : MonoBehaviour, IPointerClickHandler
+	{
+		public UIFader Fader;
+		public FadeState ActionOnClick;
+
+		public void OnPointerClick(PointerEventData eventData)
+		{
+			if (!Fader)
+				return;
+
+			switch (ActionOnClick)
+			{
+				case FadeState.FadedIn:
+					Fader.FadeIn();
+					break;
+				case FadeState.FadedOut:
+					Fader.FadeOut();
+					break;
+			}
+		}
+	}
+
+}
