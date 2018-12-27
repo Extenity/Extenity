f197f4bbc9318fc8eca442a145bb8926bada692b
Can Baycay <can.baycay@gmail.com>
2018-12-28 02:44:49 +0300

    Add: ButtonHitZoneSizing

--- Scripts/Runtime/UI/ButtonHitZone.cs
+++ Scripts/Runtime/UI/ButtonHitZone.cs
@@ -1,8 +1,16 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace Extenity.UIToolbox
 {
+
+	public enum ButtonHitZoneSizing
+	{
+		Exact,
+		AdditivePixels,
+		AdditiveProportional,
+	}
 
 	public class ButtonHitZone : MonoBehaviour
 	{
@@ -17,6 +25,7 @@ namespace Extenity.UIToolbox
 
 		#region Hit Zone
 
+		public ButtonHitZoneSizing Sizing = ButtonHitZoneSizing.Exact;
 		public float Width;
 		public float Height;
 
@@ -27,7 +36,20 @@ namespace Extenity.UIToolbox
 			hitZoneTransform.SetParent(transform);
 			hitZoneTransform.localPosition = Vector3.zero;
 			hitZoneTransform.localScale = Vector3.one;
-			hitZoneTransform.sizeDelta = new Vector2(Width, Height);
+			switch (Sizing)
+			{
+				case ButtonHitZoneSizing.Exact:
+					hitZoneTransform.sizeDelta = new Vector2(Width, Height);
+					break;
+				case ButtonHitZoneSizing.AdditivePixels:
+					hitZoneTransform.sizeDelta = GetComponent<RectTransform>().sizeDelta + new Vector2(Width, Height);
+					break;
+				case ButtonHitZoneSizing.AdditiveProportional:
+					hitZoneTransform.sizeDelta = Vector2.Scale(GetComponent<RectTransform>().sizeDelta, new Vector2(Width, Height));
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
 
 			// create transparent graphic
 			hitZoneGameObject.AddComponent<DummyGraphic>();
@@ -59,11 +81,32 @@ namespace Extenity.UIToolbox
 
 		private void OnValidate()
 		{
+			// Make sure the hit area size is not lesser than the button's actual size.
+			// Why anyone would want to do that? If a reason pops up in future, just delete these lines and move on.
 			var rectTransform = GetComponent<RectTransform>();
 			if (rectTransform != null)
 			{
-				Width = Mathf.Max(Width, rectTransform.sizeDelta.x);
-				Height = Mathf.Max(Height, rectTransform.sizeDelta.y);
+				switch (Sizing)
+				{
+					case ButtonHitZoneSizing.Exact:
+						{
+							Width = Mathf.Max(Width, rectTransform.sizeDelta.x);
+							Height = Mathf.Max(Height, rectTransform.sizeDelta.y);
+						}
+						break;
+					case ButtonHitZoneSizing.AdditivePixels:
+						{
+							if (Width < 0f) Width = 0f;
+							if (Height < 0f) Height = 0f;
+						}
+						break;
+					case ButtonHitZoneSizing.AdditiveProportional:
+						{
+							if (Width < 1f) Width = 1f;
+							if (Height < 1f) Height = 1f;
+						}
+						break;
+				}
 			}
 		}
 
