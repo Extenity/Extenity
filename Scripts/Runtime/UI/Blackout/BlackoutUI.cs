9fd804197e440c97d9c9298a4b5312258fdd445d
Can Baycay <can.baycay@gmail.com>
2019-02-12 20:43:40 +0300

    Refactor: BlackoutUI moved into Extenity.

---
+++ Scripts/Runtime/UI/Blackout/BlackoutUI.cs
@@ -0,0 +1,56 @@
+﻿using Extenity.DesignPatternsToolbox;
+using Extenity.UIToolbox;
+
+namespace WheelsOfSteel
+{
+
+	// TODO: Move into Extenity
+	public class BlackoutUI : SingletonUnity<BlackoutUI>
+	{
+		#region Initialization
+
+		private void Awake()
+		{
+			InitializeSingleton(true);
+		}
+
+		#endregion
+
+		#region Fade
+
+		// TODO: Make it count the Show and Hide requests and act accordingly, i.e. hide only if all the Show requests canceled out with a respective Hide request.
+
+		public UIFader Fader;
+
+		public void Blackout(bool immediate = false)
+		{
+			//Log.Info("Activating blackout" + (immediate ? " immediately" : ""));
+
+			if (immediate)
+			{
+				Fader.FadeInImmediate();
+			}
+			else
+			{
+				Fader.FadeIn();
+			}
+		}
+
+		public void Clear(bool immediate = false)
+		{
+			//Log.Info("Clearing blackout" + (immediate ? " immediately" : ""));
+
+			if (immediate)
+			{
+				Fader.FadeOutImmediate();
+			}
+			else
+			{
+				Fader.FadeOut();
+			}
+		}
+
+		#endregion
+	}
+
+}
