69dede02f777674bc28c96f1d6b950227b597dc6
Can Baycay <can.baycay@gmail.com>
2019-02-16 01:57:19 +0300

    Add: EditorWindow.IsDockedAndHidden

--- Scripts/Editor/Core/UnityEditor/EditorWindowTools.cs
+++ Scripts/Editor/Core/UnityEditor/EditorWindowTools.cs
@@ -18,6 +18,26 @@ namespace Extenity.UnityEditorToolbox.Editor
 		}
 
 		#endregion
+
+		#region EditorWindow.hasFocus
+
+		private static PropertyInfo HasFocusPropertyInfo;
+
+		/// <summary>
+		/// Tells if the window is docked and another tab is active where it's docked. Returns false for floating windows.
+		/// </summary>
+		public static bool IsDockedAndHidden(this EditorWindow window)
+		{
+			// EditorWindow.hasFocus property is working in mysterious ways.
+			// I think it's not named correctly. Tells 'true' for floating
+			// windows, even when the window has no focus. So we use 'hasFocus'
+			// with a more meaningful name as 'IsDockedAndHidden'.
+			if (HasFocusPropertyInfo == null)
+				HasFocusPropertyInfo = typeof(EditorWindow).GetProperty("hasFocus", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetProperty);
+			return !(bool)HasFocusPropertyInfo.GetValue(window, null);
+		}
+
+		#endregion
 	}
 
 }
