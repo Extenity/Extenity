b8be5cef0b2cdb3c777c8d5567cbb7529f7c8498
Can Baycay <can.baycay@gmail.com>
2019-03-25 18:46:39 +0300

    Add: BlackoutUI improvements.

--- Scripts/Runtime/UI/Blackout/BlackoutUI.cs
+++ Scripts/Runtime/UI/Blackout/BlackoutUI.cs
@@ -1,4 +1,7 @@
-﻿using Extenity.DesignPatternsToolbox;
+﻿//#define EnableBlackoutUILog
+
+using System.Diagnostics;
+using Extenity.DesignPatternsToolbox;
 
 namespace Extenity.UIToolbox
 {
@@ -10,42 +13,92 @@ namespace Extenity.UIToolbox
 		private void Awake()
 		{
 			InitializeSingleton(true);
+
+			InitializeDebug();
 		}
 
 		#endregion
 
 		#region Fade
 
-		// TODO: Make it count the Show and Hide requests and act accordingly, i.e. hide only if all the Show requests canceled out with a respective Hide request.
-
 		public UIFader Fader;
+
+		// TODO: Decided not to go that way. Instead of using a BoolCounter, use a FlaggedBool.
+		//private readonly BoolCounter RequestCounter = new BoolCounter();
 
 		public void Blackout(bool immediate = false)
 		{
-			//Log.Info("Activating blackout" + (immediate ? " immediately" : ""));
+			//var justSwitchedOn = RequestCounter.Increase();
+			//Info($"Blackout '{(immediate ? "immediate" : "non-immediate")}' activation requested ({RequestCounter.Counter.ToStringWithEnglishPluralPostfix("active request")})");
 
-			if (immediate)
+			Info($"Blackout '{(immediate ? "immediate" : "non-immediate")}' activation requested");
+
+			//if (justSwitchedOn) // Do not apply fading on consecutive calls.
 			{
-				Fader.FadeInImmediate();
-			}
-			else
-			{
-				Fader.FadeIn();
+				if (immediate)
+				{
+					Fader.FadeInImmediate();
+				}
+				else
+				{
+					Fader.FadeIn();
+				}
 			}
 		}
 
 		public void Clear(bool immediate = false)
 		{
-			//Log.Info("Clearing blackout" + (immediate ? " immediately" : ""));
+			//var justSwitchedOff = RequestCounter.Decrease();
+			//Info($"Blackout '{(immediate ? "immediate" : "non-immediate")}' clear requested ({RequestCounter.Counter.ToStringWithEnglishPluralPostfix("active request")})");
 
-			if (immediate)
+			Info($"Blackout '{(immediate ? "immediate" : "non-immediate")}' clear requested");
+
+			//if (justSwitchedOff) // Do not apply fading on consecutive calls.
 			{
-				Fader.FadeOutImmediate();
+				if (immediate)
+				{
+					Fader.FadeOutImmediate();
+				}
+				else
+				{
+					Fader.FadeOut();
+				}
 			}
-			else
+		}
+
+		#endregion
+
+		#region Editor
+
+#if UNITY_EDITOR
+		protected void OnValidate()
+		{
+			if (Fader)
 			{
-				Fader.FadeOut();
+				if (Fader.FadeInDelay != 0f || Fader.FadeOutDelay != 0f)
+				{
+					UnityEditor.Undo.RecordObject(Fader, "Blackout fader delay correction");
+					Log.Warning("Blackout UI is expected to be launched immediately. So fader delays are not allowed. Fixing...", this);
+					Fader.FadeInDelay = 0f;
+					Fader.FadeOutDelay = 0f;
+				}
 			}
+		}
+#endif
+
+		#endregion
+
+		#region Debug
+
+		private void InitializeDebug()
+		{
+			Log.RegisterPrefix(this, "BLK");
+		}
+
+		[Conditional("EnableBlackoutUILog")]
+		private void Info(string message)
+		{
+			Log.Info(message, this);
 		}
 
 		#endregion
