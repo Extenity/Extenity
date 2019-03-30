cc6d0200771a10b767bb9ad5408ec09c9ba6ebd7
Can Baycay <can.baycay@gmail.com>
2019-03-30 13:13:11 +0300

    Refactor: Move.

--- Scripts/Editor/BuildMachine/BuildProcessor/BuildProcessorCallbacks.cs
+++ Scripts/Editor/BuildMachine/BuildProcessor/BuildProcessorCallbacks.cs
@@ -2,17 +2,13 @@ using Extenity.BuildToolbox.Editor;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
-using UnityEngine.SceneManagement;
 
 namespace Extenity.BuildMachine.Editor
 {
 
-	// TODO: Detect and throw error if any changes made that needs recompilation during scene processes. Try to use CompilationPipeline.assemblyCompilationStarted. See 112739521.
-
 	public class BuildProcessorCallbacks :
 		IPreprocessBuildWithReport,
-		IPostprocessBuildWithReport,
-		IProcessSceneWithReport // TODO: Move this into SceneProcessorCallbacks
+		IPostprocessBuildWithReport
 	{
 		public int callbackOrder => 10000;
 
@@ -35,20 +31,6 @@ namespace Extenity.BuildMachine.Editor
 		public void OnPostprocessBuild(BuildReport report)
 		{
 			Log.Info($"Build processor checking in at postprocess callback... Report details: " + report.ToDetailedLogString());
-		}
-
-		public void OnProcessScene(Scene scene, BuildReport report)
-		{
-			if (EditorApplication.isPlayingOrWillChangePlaymode)
-			{
-				// TODO: Automatically processing the scene when pressing the Play button should be made here. But processing the scene each time the Play button gets pressed is madness. So before doing that, a mechanism for checking if a process is already done before should be implemented first.
-				return;
-			}
-
-			Log.Info($"Build processor checking in at scene process callback for '{scene.name}'.");
-
-			// See 713951791.
-			//DoProcessScene(scene, ...);
 		}
 	}
 
---
+++ Scripts/Editor/BuildMachine/SceneProcessor/SceneProcessorCallbacks.cs
@@ -0,0 +1,31 @@
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine.SceneManagement;
+
+namespace Extenity.BuildMachine.Editor
+{
+
+	// TODO: Detect and throw error if any changes made that needs recompilation during scene processes. Try to use CompilationPipeline.assemblyCompilationStarted. See 112739521.
+
+	public class SceneProcessorCallbacks :
+		IProcessSceneWithReport
+	{
+		public int callbackOrder => 10000;
+
+		public void OnProcessScene(Scene scene, BuildReport report)
+		{
+			if (EditorApplication.isPlayingOrWillChangePlaymode)
+			{
+				// TODO: Automatically processing the scene when pressing the Play button should be made here. But processing the scene each time the Play button gets pressed is madness. So before doing that, a mechanism for checking if a process is already done before should be implemented first.
+				return;
+			}
+
+			Log.Info($"Build processor checking in at scene process callback for '{scene.name}'.");
+
+			// See 713951791.
+			//DoProcessScene(scene, ...);
+		}
+	}
+
+}
