16048ecda4ef3c839b7fada400f2a85eea7062b5
Can Baycay <can.baycay@gmail.com>
2019-05-22 02:12:47 +0300

    Add: BuildProcessorManager checks for non-serialized fields.

--- Scripts/Editor/BuildMachine/BuildProcessor/BuildProcessorManager.cs
+++ Scripts/Editor/BuildMachine/BuildProcessor/BuildProcessorManager.cs
@@ -43,7 +43,18 @@ namespace Extenity.BuildMachine.Editor
 				var attribute = type.GetAttribute<BuildProcessorInfoAttribute>(true);
 				if (attribute == null)
 				{
-					throw new Exception($"Build processor '{type.Name}' has no '{nameof(BuildProcessorInfoAttribute)}'.");
+					Log.Error($"Build processor '{type.Name}' has no '{nameof(BuildProcessorInfoAttribute)}'.");
+				}
+
+				// Serializable fields
+				var nonSerializedFields = type
+					.GetNonSerializedFields()
+					.Where(field => !field.Name.StartsWith("_"))
+					.ToArray();
+				if (nonSerializedFields.Length > 0)
+				{
+					var text = string.Join(", ", nonSerializedFields.Select(entry => entry.Name));
+					Log.Error($"Build processor '{type.Name}' has non-serializable field(s) '{text}' which is not allowed to prevent any confusion. Builders need to be fully serializable to prevent losing data between assembly reloads and Unity Editor relaunches. Start the name with '_' to ignore this check if the non-serialized field is essential.");
 				}
 
 				buildProcessors[i] = new BuildProcessorDefinition(attribute.Name, type);
