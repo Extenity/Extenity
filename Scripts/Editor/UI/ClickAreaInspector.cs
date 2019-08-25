1805d6dd7d0e0fd8eee97019df9ffa04f88a987b
Can Baycay <can.baycay@gmail.com>
2019-08-25 22:18:48 +0300

    Change: ButtonHitZone removed and DummyGraphic turned into ClickArea. Add: Right click menu item to create ClickArea. Add: Ellipse shape for ClickArea.

---
+++ Scripts/Editor/UI/ClickAreaInspector.cs
@@ -0,0 +1,80 @@
+using Extenity.UnityEditorToolbox.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Extenity.UIToolbox.Editor
+{
+
+	[CustomEditor(typeof(ClickArea))]
+	public class ClickAreaInspector : ExtenityEditorBase<ClickArea>
+	{
+		private SerializedProperty RaycastTargetProperty;
+		private SerializedProperty ShapeProperty;
+
+		protected override void OnEnableDerived()
+		{
+			IsDefaultInspectorDrawingEnabled = false;
+			ShapeProperty = GetProperty("Shape");
+			RaycastTargetProperty = GetProperty("m_RaycastTarget");
+		}
+
+		protected override void OnDisableDerived()
+		{
+		}
+
+		protected override void OnAfterDefaultInspectorGUI()
+		{
+			EditorGUILayout.PropertyField(ShapeProperty);
+			EditorGUILayout.PropertyField(RaycastTargetProperty);
+		}
+
+		#region Editor right Click Menu
+
+#if UNITY_EDITOR
+
+		[MenuItem("GameObject/UI/Click Area", false, 2100 - 3)] // Just above EventSystem
+		private static void AddClickArea(MenuCommand menuCommand)
+		{
+			var go = new GameObject(nameof(ClickArea));
+			go.AddComponent<ClickArea>();
+
+			UIEditorUtilities.PlaceUIElementRoot(go, menuCommand);
+
+			var defaultOffset = new Vector2(30, 30);
+			var rectTransform = go.GetComponent<RectTransform>();
+			rectTransform.anchorMin = Vector2.zero;
+			rectTransform.anchorMax = Vector2.one;
+			rectTransform.anchoredPosition = Vector2.zero;
+			rectTransform.offsetMin = -defaultOffset;
+			rectTransform.offsetMax = defaultOffset;
+
+			// Disable RaycastTarget of parent object
+			var parentGO = menuCommand.context as GameObject;
+			if (parentGO != null)
+			{
+				var monoBehaviours = parentGO.GetComponents<MonoBehaviour>();
+				foreach (var monoBehaviour in monoBehaviours)
+				{
+					if (monoBehaviour == null) // There might be missing scripts.
+						continue;
+					var serializedObject = new SerializedObject(monoBehaviour);
+					var raycastTargetProperty = serializedObject.FindProperty("m_RaycastTarget");
+					if (raycastTargetProperty != null && raycastTargetProperty.propertyType == SerializedPropertyType.Boolean)
+					{
+						if (raycastTargetProperty.boolValue)
+						{
+							Undo.RecordObject(monoBehaviour, "Raycast target disabled");
+							raycastTargetProperty.boolValue = false;
+							serializedObject.ApplyModifiedProperties();
+						}
+					}
+				}
+			}
+		}
+
+#endif
+
+		#endregion
+	}
+
+}
---
+++ Scripts/Runtime/UI/ClickArea.cs
@@ -0,0 +1,94 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Extenity.UIToolbox
+{
+
+	public enum ClickAreaShape
+	{
+		Square,
+		Ellipse,
+	}
+
+	/// <summary>
+	/// Transparent, clickable-only UI element.
+	/// </summary>
+	public class ClickArea : Graphic, ICanvasRaycastFilter
+	{
+		public ClickAreaShape Shape = ClickAreaShape.Square;
+
+		public override void Rebuild(CanvasUpdate update)
+		{
+			// We do nothing. So UpdateMaterial and UpdateGeometry won't be called.
+		}
+
+		protected override void OnPopulateMesh(VertexHelper vertexHelper)
+		{
+			vertexHelper.Clear();
+		}
+
+		public bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+		{
+			var insideRect = RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, screenPoint, eventCamera, out _);
+
+			switch (Shape)
+			{
+				case ClickAreaShape.Square:
+					return insideRect;
+
+				case ClickAreaShape.Ellipse:
+				{
+					if (insideRect)
+					{
+						if (RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, screenPoint, eventCamera, out var localPoint))
+						{
+							var rect = rectTransform.rect;
+							var diff = rect.center - localPoint;
+							diff.x /= rect.width;
+							diff.y /= rect.height;
+							return diff.sqrMagnitude <= 0.5f * 0.5f; // 0.5 is the radius of unit circle.
+						}
+					}
+					return false;
+				}
+
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+
+		#region Override other Graphic methods for clarity
+
+		protected override void UpdateMaterial()
+		{
+		}
+
+		protected override void UpdateGeometry()
+		{
+		}
+
+		public override void CrossFadeColor(Color targetColor, float duration, bool ignoreTimeScale, bool useAlpha)
+		{
+		}
+
+		public override void CrossFadeColor(Color targetColor, float duration, bool ignoreTimeScale, bool useAlpha, bool useRGB)
+		{
+		}
+
+		public override void CrossFadeAlpha(float alpha, float duration, bool ignoreTimeScale)
+		{
+		}
+
+		public override Material material
+		{
+			get => null;
+			set { }
+		}
+		public override Material materialForRendering => null;
+		public override Texture mainTexture => null;
+
+		#endregion
+	}
+
+}
