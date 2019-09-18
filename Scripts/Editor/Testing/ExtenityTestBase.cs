00463d2337c129e927896865b721365f447fa3c9
Can Baycay <can.baycay@gmail.com>
2019-09-18 16:54:01 +0300

    Add: ExtenityTestBase

---
+++ Scripts/Editor/Testing/ExtenityTestBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Extenity.DataToolbox;
+using Extenity.UnityTestToolbox;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Extenity.Testing
+{
+
+	public abstract class ExtenityTestBase : AssertionHelper
+	{
+		#region Initialization
+
+		protected virtual void OnInitialize()
+		{
+		}
+
+		[SetUp]
+		public void Initialize()
+		{
+			StartTime = Time.realtimeSinceStartup;
+
+			OnInitialize();
+		}
+
+		#endregion
+
+		#region Deinitialization
+
+		protected virtual void OnDeinitialize()
+		{
+		}
+
+		[TearDown]
+		public void Deinitialize()
+		{
+			OnDeinitialize();
+
+			UnityTestTools.Cleanup();
+		}
+
+		#endregion
+
+		#region Timing
+
+		protected float StartTime;
+		protected float PassedTimeThreshold = 3f;
+		protected float PassedTime => Time.realtimeSinceStartup - StartTime;
+
+		protected void CheckPassedTestTimeThreshold()
+		{
+			if (PassedTime > PassedTimeThreshold)
+				throw new Exception("Test taking too long.");
+		}
+
+		protected void LogPassedTime()
+		{
+			Log.Info("Passed time: " + TimeSpan.FromSeconds(PassedTime).ToStringMinutesSecondsMilliseconds());
+		}
+
+		#endregion
+	}
+
+}
