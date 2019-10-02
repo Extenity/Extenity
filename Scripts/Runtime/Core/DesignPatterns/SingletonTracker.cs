67bb3fec3eb9db83a7efa56f85a3fd2715451e2f
Can Baycay <can.baycay@gmail.com>
2019-10-02 16:18:45 +0300

    Add: SingletonTracker improvements.

--- Scripts/Runtime/Core/DesignPatterns/SingletonTracker.cs
+++ Scripts/Runtime/Core/DesignPatterns/SingletonTracker.cs
@@ -1,85 +1,40 @@
 using System.Collections.Generic;
-using System.Diagnostics;
 
 namespace Extenity.DesignPatternsToolbox
 {
 
-	internal class SingletonTracker
+	internal static class SingletonTracker
 	{
 		#region Singleton Tracker
 
-		private static Dictionary<string, int> singletonCalls = new Dictionary<string, int>();
+		private static Dictionary<string, int> _SingletonCalls = new Dictionary<string, int>();
+		public static Dictionary<string, int> SingletonCalls => _SingletonCalls;
 
-		//public static Dictionary<string, int> SingletonCalls
-		//{
-		//	get { return singletonCalls; }
-		//}
-
-		[Conditional("UNITY_EDITOR"), Conditional("DEBUG")]
-		public static void SingletonInstantiated()
-		{
-			SingletonInstantiated(PreviousMethodType);
-		}
-
-		[Conditional("UNITY_EDITOR"), Conditional("DEBUG")]
 		public static void SingletonInstantiated(string className)
 		{
-			if (singletonCalls.ContainsKey(className))
+			if (_SingletonCalls.ContainsKey(className))
 			{
-				singletonCalls[className]++;
-				if (singletonCalls[className] > 1)
+				_SingletonCalls[className]++;
+				if (_SingletonCalls[className] > 1)
 				{
-					Log.Error("Singleton '" + className + "' instantiated " + singletonCalls[className] + " times");
+					Log.Error("Singleton '" + className + "' instantiated " + _SingletonCalls[className] + " times");
 				}
 			}
 			else
-				singletonCalls.Add(className, 1);
+			{
+				_SingletonCalls.Add(className, 1);
+			}
 		}
 
-		[Conditional("UNITY_EDITOR"), Conditional("DEBUG")]
-		public static void SingletonDestroyed()
-		{
-			SingletonDestroyed(PreviousMethodType);
-		}
-
-		[Conditional("UNITY_EDITOR"), Conditional("DEBUG")]
 		public static void SingletonDestroyed(string className)
 		{
-			if (singletonCalls.ContainsKey(className))
-				singletonCalls[className]--;
-			else
-				Log.Error("Unregistered singleton: " + className);
-		}
-
-		//[Conditional("UNITY_EDITOR"), Conditional("DEBUG")]
-		//public static void DrawSingletonTrackerDebugGUI()
-		//{
-		//	int lineHeight = 14;
-
-		//	Rect rect1 = new Rect(100, 20, 210, 20);
-		//	Rect rect2 = new Rect(320, 20, 100, 20);
-
-		//	GUI.Label(new Rect(130, 5, 160, 20), "Singleton Tracker");
-
-		//	foreach (KeyValuePair<string, int> pair in singletonCalls)
-		//	{
-		//		GUI.Label(rect1, pair.Key);
-		//		GUI.Label(rect2, pair.Value.ToString());
-		//		rect1.y += lineHeight;
-		//		rect2.y += lineHeight;
-		//	}
-		//}
-
-		#endregion
-
-		#region Tools
-
-		private static string PreviousMethodType
-		{
-			get
+			if (_SingletonCalls.ContainsKey(className))
 			{
-				StackFrame stackframe = new StackFrame(2, true);
-				return stackframe.GetMethod().ReflectedType.Name;
+				_SingletonCalls[className]--;
+			}
+			else
+			{
+				Log.Error("Unregistered singleton: " + className);
 			}
 		}
 
