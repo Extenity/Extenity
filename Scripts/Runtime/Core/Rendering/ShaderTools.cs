c46fe1af206449049b575b859758d58aea560f82
Can Baycay <can.baycay@gmail.com>
2019-10-07 20:49:05 +0300

    Add: ShaderTools for common properties.

---
+++ Scripts/Runtime/Core/Rendering/ShaderTools.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace Extenity.RenderingToolbox
+{
+
+	public static class ShaderTools
+	{
+		#region Common Shader Properties
+
+		private static int _BaseColorProperty = 0;
+		public static int BaseColorProperty
+		{
+			get
+			{
+				if (_BaseColorProperty == 0)
+				{
+					_BaseColorProperty = Shader.PropertyToID("_BaseColor");
+				}
+				return _BaseColorProperty;
+			}
+		}
+
+		private static int _OffsetProperty = 0;
+		public static int OffsetProperty
+		{
+			get
+			{
+				if (_OffsetProperty == 0)
+				{
+					_OffsetProperty = Shader.PropertyToID("_Offset");
+				}
+				return _OffsetProperty;
+			}
+		}
+
+		#endregion
+	}
+
+}
