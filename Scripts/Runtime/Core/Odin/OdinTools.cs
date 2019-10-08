d4887dec104929657ae6ce347876ded3e843518f
Can Baycay <can.baycay@gmail.com>
2019-10-08 17:47:30 +0300

    Add: OdinTools.Repaint

---
+++ Scripts/Runtime/Core/Odin/OdinTools.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+
+namespace Extenity.OdinToolbox
+{
+
+	public static class OdinTools
+	{
+		/// <summary>
+		/// Tells Odin to repaint the inspector. See example below for continuous repaint.
+		/// </summary>
+		/// <example>
+		/// <code>
+		/// #if UNITY_EDITOR
+		/// 	[OnInspectorGUI]
+		/// 	private void OnInspectorGUI()
+		/// 	{
+		/// 		OdinTools.Repaint();
+		/// 	}
+		/// #endif
+		/// </code>
+		/// </example>
+		[Conditional("ODIN_INSPECTOR")]
+		public static void Repaint()
+		{
+#if ODIN_INSPECTOR
+			Sirenix.Utilities.Editor.GUIHelper.RequestRepaint();
+#endif
+		}
+	}
+
+}
