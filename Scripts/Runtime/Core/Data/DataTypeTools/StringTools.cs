08124540613b346ca10610bc96e617ee7b7e2611
Can Baycay <can.baycay@gmail.com>
2020-05-24 00:57:29 +0300

    Add: int.ToStringClampedBetween0To99

--- Scripts/Runtime/Core/Data/DataTypeTools/StringTools.cs
+++ Scripts/Runtime/Core/Data/DataTypeTools/StringTools.cs
@@ -717,6 +717,51 @@ namespace Extenity.DataToolbox
 
 		#endregion
 
+		#region Conversions - Predefined Numbers
+
+		public static readonly string[] NumbersFrom0To9 =
+		{
+			"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+		};
+
+		public static readonly string[] NumbersFrom00To09 =
+		{
+			"00", "01", "02", "03", "04", "05", "06", "07", "08", "09",
+		};
+
+		public static readonly string[] NumbersFrom10To99 =
+		{
+			"10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+			"20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+			"30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
+			"40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
+			"50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
+			"60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
+			"70", "71", "72", "73", "74", "75", "76", "77", "78", "79",
+			"80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
+			"90", "91", "92", "93", "94", "95", "96", "97", "98", "99"
+		};
+
+		public static string ToStringClampedBetween0To99(this int value)
+		{
+			value = Mathf.Clamp(value, 0, 99);
+			if (value < 10)
+				return NumbersFrom0To9[value];
+			else
+				return NumbersFrom10To99[value - 10];
+		}
+
+		public static string ToStringClampedBetween00To99(this int value)
+		{
+			value = Mathf.Clamp(value, 0, 99);
+			if (value < 10)
+				return NumbersFrom00To09[value];
+			else
+				return NumbersFrom10To99[value - 10];
+		}
+
+		#endregion
+
 		#region Conversions - Int32 ToStringAsCharArray
 
 		private static void _ToStringAsCharArray(Int32 value, char[] array, int endOffset, out int startIndex, out int length)
