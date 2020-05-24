dba477f26dc0baadc6f131795107181cd97ffff4
Can Baycay <can.baycay@gmail.com>
2020-05-24 14:35:51 +0300

    Add: StandaloneImprovedInputModule

---
+++ Scripts/Runtime/UI/InputModule/StandaloneImprovedInputModule.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Extenity.UIToolbox
+{
+
+	public class StandaloneImprovedInputModule : StandaloneInputModule
+	{
+		#region CurrentInput
+
+		private static StandaloneImprovedInputModule _CurrentInput;
+		public static StandaloneImprovedInputModule CurrentInput
+		{
+			get
+			{
+				if (_CurrentInput == null)
+				{
+					var current = EventSystem.current;
+					if (current != null)
+					{
+						_CurrentInput = current.currentInputModule as StandaloneImprovedInputModule;
+					}
+					if (_CurrentInput == null)
+					{
+						Log.Error($"{nameof(StandaloneImprovedInputModule)} is expected.");
+					}
+				}
+				return _CurrentInput;
+			}
+		}
+
+		#endregion
+
+		#region IsPointerOverGameObject
+
+		public bool IsPointerOverGameObject()
+		{
+			return IsPointerOverGameObject(kMouseLeftId);
+		}
+
+		#endregion
+
+		#region GameObjectUnderPointer
+
+		/// <summary>
+		/// Source: https://stackoverflow.com/questions/39150165/how-do-i-find-which-object-is-eventsystem-current-ispointerovergameobject-detect
+		/// </summary>
+		public GameObject GameObjectUnderPointer(int pointerId)
+		{
+			var lastPointer = GetLastPointerEventData(pointerId);
+			if (lastPointer != null)
+				return lastPointer.pointerCurrentRaycast.gameObject;
+			return null;
+		}
+
+		/// <summary>
+		/// Source: https://stackoverflow.com/questions/39150165/how-do-i-find-which-object-is-eventsystem-current-ispointerovergameobject-detect
+		/// </summary>
+		public GameObject GameObjectUnderPointer()
+		{
+			return GameObjectUnderPointer(kMouseLeftId);
+		}
+
+		#endregion
+	}
+
+}
