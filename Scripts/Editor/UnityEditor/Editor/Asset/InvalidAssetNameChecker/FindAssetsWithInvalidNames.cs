26d909e2d924257fef3defc86074657f04a34af6
Can Baycay <can.baycay@gmail.com>
2020-05-26 17:49:19 +0300

    Add: Invalid asset name checker.

---
+++ Scripts/Editor/UnityEditor/Editor/Asset/InvalidAssetNameChecker/FindAssetsWithInvalidNames.cs
@@ -0,0 +1,83 @@
+using System.Linq;
+using Extenity.DataToolbox;
+using UnityEditor;
+
+namespace Extenity.AssetToolbox.Editor
+{
+
+	public static class FindAssetsWithInvalidNames
+	{
+		#region Configuration
+
+		/// <summary>
+		/// File paths matching this filter will be excluded from invalid name checking. Feel free to modify this filter
+		/// for project specific needs.
+		/// </summary>
+		public static StringFilter ExcludeFilter = new StringFilter(
+			// new StringFilterEntry(StringFilterType.StartsWith, "Packages/"), Commented out for future needs. Don't hesitate to exclude files in Packages directory if required. Packages are expected to be tested and working alright.
+			new StringFilterEntry(StringFilterType.StartsWith, "C:") // Exclude Unity installation files. // TODO: Find a cross-platform supported, not-hardcoded approach.
+			);
+
+		private const string ValidCharactersString = "0123456789" + "/_.-+&()@ " + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz";
+		private static readonly char[] ValidCharacters = ValidCharactersString.ToCharArray();
+
+		#endregion
+
+		#region Menu
+
+		[MenuItem("Assets/Find Assets With Invalid Names")]
+		public static void _FindAssetsWithInvalidNames()
+		{
+			Run();
+		}
+
+		#endregion
+
+		#region Run
+
+		public static void Run()
+		{
+			var assetPaths = AssetDatabase.GetAllAssetPaths()
+			                              .Where(ShouldCheckFileAtPath)
+			                              .ToArray();
+			var found = false;
+
+			for (int i = 0; i < assetPaths.Length; i++)
+			{
+				if (ContainsInvalidChar(assetPaths[i]))
+				{
+					Log.Warning($"{assetPaths[i]} asset has invalid name.");
+					found = true;
+				}
+			}
+
+			if (!found)
+			{
+				Log.Info($"All {assetPaths.Length} asset names are valid.");
+			}
+		}
+
+		#endregion
+
+		#region Operations
+
+		public static bool ShouldCheckFileAtPath(string path)
+		{
+			return !ExcludeFilter.IsMatching(path);
+		}
+
+		public static bool ContainsInvalidChar(string path)
+		{
+			for (var i = 0; i < path.Length; i++)
+			{
+				if (!ValidCharacters.Contains(path[i]))
+					return true;
+			}
+
+			return false;
+		}
+
+		#endregion
+	}
+
+}
---
+++ Scripts/Editor/UnityEditor/Editor/Asset/InvalidAssetNameChecker/FindAssetsWithInvalidNamesAssetPostProcessor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+
+namespace Extenity.AssetToolbox.Editor
+{
+
+	public class FindAssetsWithInvalidNamesAssetPostProcessor : AssetPostprocessor
+	{
+		private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+		{
+			for (var i = 0; i < importedAssets.Length; i++)
+			{
+				DisplayDialogIfInvalid(importedAssets[i]);
+			}
+			for (var i = 0; i < movedAssets.Length; i++)
+			{
+				DisplayDialogIfInvalid(movedAssets[i]);
+			}
+		}
+
+		private static void DisplayDialogIfInvalid(string path)
+		{
+			if (FindAssetsWithInvalidNames.ShouldCheckFileAtPath(path) &&
+			    FindAssetsWithInvalidNames.ContainsInvalidChar(path))
+			{
+				EditorUtility.DisplayDialog("ATTENTION!", $"File name has invalid characters:\n\n{path}\n\nPlease fix the naming before it causes Unity or Repository to implode.", "Ok! Understood!");
+				Selection.activeObject = AssetDatabase.LoadAssetAtPath(path, typeof(UnityEngine.Object));
+			}
+		}
+	}
+
+}
