b2187065ba7c736985dd72506b436199dc4c56f8
Can Baycay <can.baycay@gmail.com>
2020-06-02 06:00:51 +0300

    Refactor: Move.

---
+++ Scripts/Runtime/Core/Flow/CachedPoller.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Extenity.FlowToolbox
+{
+
+	public class CachedPoller<TResult>
+	{
+		#region Initialization
+
+		public CachedPoller(float interval)
+		{
+			Interval = interval;
+		}
+
+		#endregion
+
+		#region Result
+
+		TResult _CachedResult;
+		public TResult CachedResult
+		{
+			get => _CachedResult;
+			set
+			{
+				_CachedResult = value;
+				NextProcessTime = Time.realtimeSinceStartup + Interval;
+			}
+		}
+
+		#endregion
+
+		#region Timing
+
+		public float NextProcessTime = float.MinValue;
+		public float Interval;
+
+		public bool IsTimeToProcess => NextProcessTime < Time.realtimeSinceStartup;
+
+		public void Invalidate()
+		{
+			NextProcessTime = float.MinValue;
+		}
+
+		#endregion
+	}
+
+}
