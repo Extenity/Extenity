68b35a4180d45a1c218755b662d624280e576b25
Can Baycay <can.baycay@gmail.com>
2020-06-21 15:48:56 +0300

    Add: TaggedTransform and TaggedCollider.

---
+++ Scripts/Runtime/Core/GameObject/TaggedCollider.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+namespace Extenity.GameObjectToolbox
+{
+
+	[Serializable]
+	public struct TaggedCollider
+	{
+		public string Tag;
+
+		private Collider _Collider;
+		public Collider Collider
+		{
+			get
+			{
+				if (!_Collider)
+				{
+					var go = GameObjectTools.FindSingleObjectWithTagEnsured(Tag);
+					_Collider = go.GetComponent<Collider>();
+				}
+				return _Collider;
+			}
+		}
+	}
+
+}
---
+++ Scripts/Runtime/Core/GameObject/TaggedTransform.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+namespace Extenity.GameObjectToolbox
+{
+
+	[Serializable]
+	public struct TaggedTransform
+	{
+		public string Tag;
+
+		private Transform _Transform;
+		public Transform Transform
+		{
+			get
+			{
+				if (!_Transform)
+				{
+					var go = GameObjectTools.FindSingleObjectWithTagEnsured(Tag);
+					_Transform = go.transform;
+				}
+				return _Transform;
+			}
+		}
+	}
+
+}
