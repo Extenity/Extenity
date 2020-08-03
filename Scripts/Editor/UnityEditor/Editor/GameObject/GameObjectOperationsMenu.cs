62dd1c55c741b242c14098194f80713b37316ba6
Can Baycay <can.baycay@gmail.com>
2020-08-03 18:18:49 +0300

    Refactor: Unused code cleanup.

--- Scripts/Editor/UnityEditor/Editor/GameObject/GameObjectOperationsMenu.cs
+++ Scripts/Editor/UnityEditor/Editor/GameObject/GameObjectOperationsMenu.cs
@@ -11,30 +11,6 @@ namespace Extenity.GameObjectToolbox.Editor
 
 	public static class GameObjectOperationsMenu
 	{
-		// Understanding the GameObject menu
-		//[MenuItem("GameObject/XXXXXX_-100/AAA", priority = -100)] public static void XXXXX_minus_100() { }
-		//[MenuItem("GameObject/XXXXXX_-50/AAA", priority = -50)] public static void XXXXX_minus_50() { }
-		//[MenuItem("GameObject/XXXXXX_-10/AAA", priority = -10)] public static void XXXXX_minus_10() { }
-		//[MenuItem("GameObject/XXXXXX_-2/AAA", priority = -2)] public static void XXXXX_minus_2() { }
-		//[MenuItem("GameObject/XXXXXX_-1/AAA", priority = -1)] public static void XXXXX_minus_1() { }
-		//[MenuItem("GameObject/XXXXXX_0/AAA", priority = 0)] public static void XXXXX_0() { }
-		//[MenuItem("GameObject/XXXXXX_1/AAA", priority = 1)] public static void XXXXX_1() { }
-		//[MenuItem("GameObject/XXXXXX_2/AAA", priority = 2)] public static void XXXXX_2() { }
-		//[MenuItem("GameObject/XXXXXX_3/AAA", priority = 3)] public static void XXXXX_3() { }
-		//[MenuItem("GameObject/XXXXXX_4/AAA", priority = 4)] public static void XXXXX_4() { }
-		//[MenuItem("GameObject/XXXXXX_5/AAA", priority = 5)] public static void XXXXX_5() { }
-		//[MenuItem("GameObject/XXXXXX_6/AAA", priority = 6)] public static void XXXXX_6() { }
-		//[MenuItem("GameObject/XXXXXX_7/AAA", priority = 7)] public static void XXXXX_7() { }
-		//[MenuItem("GameObject/XXXXXX_8/AAA", priority = 8)] public static void XXXXX_8() { }
-		//[MenuItem("GameObject/XXXXXX_9/AAA", priority = 9)] public static void XXXXX_9() { }
-		//[MenuItem("GameObject/XXXXXX_10/AAA", priority = 10)] public static void XXXXX_10() { }
-		//[MenuItem("GameObject/XXXXXX_11/AAA", priority = 11)] public static void XXXXX_11() { }
-		//[MenuItem("GameObject/XXXXXX_12/AAA", priority = 12)] public static void XXXXX_12() { }
-		//[MenuItem("GameObject/XXXXXX_13/AAA", priority = 13)] public static void XXXXX_13() { }
-		//[MenuItem("GameObject/XXXXXX_50/AAA", priority = 50)] public static void XXXXX_50() { }
-		//[MenuItem("GameObject/XXXXXX_100/AAA", priority = 100)] public static void XXXXX_100() { }
-		//[MenuItem("GameObject/XXXXXX_1000/AAA", priority = 1000)] public static void XXXXX_1000() { }
-
 		// CAUTION! This method should be at the top of this class, above other MenuItem methods!
 		// This is just to give the menu a priority so that sub items may have their own priorities.
 		// It's not a good way, though there is no other way.
