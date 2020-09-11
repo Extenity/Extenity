6c6b046444d7098cfa1e4d444ec6169fe0d91b60
Can Baycay <can.baycay@gmail.com>
2020-09-11 17:24:56 +0300

    Add: SmoothFollow

---
+++ Scripts/Runtime/Core/Animation/ScriptedAnimations/SmoothFollow.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Extenity.AnimationToolbox.ScriptedAnimations
+{
+
+	public class SmoothFollow : MonoBehaviour
+	{
+		public Transform Target;
+		public Transform Follower;
+		public float MovementSmoothingFactor = 10f;
+
+		protected void FixedUpdate()
+		{
+			var followerPosition = Follower.position;
+			Follower.position = followerPosition + (Target.position - followerPosition) * (MovementSmoothingFactor * Time.deltaTime);
+		}
+	}
+
+}
