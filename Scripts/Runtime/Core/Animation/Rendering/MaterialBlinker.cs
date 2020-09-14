47a29f62e97c057fa2215b5acb27f7f2f109712b
Can Baycay <can.baycay@gmail.com>
2020-09-15 00:19:05 +0300

    Add: MaterialBlinker

---
+++ Scripts/Runtime/Core/Animation/Rendering/MaterialBlinker.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Extenity.AnimationToolbox
+{
+
+	public class MaterialBlinker : MonoBehaviour
+	{
+		public bool UseScaledTime = true;
+		public float BlinkInterval = 0.15f;
+		public Material MaterialA;
+		public Material MaterialB;
+		public MeshRenderer MeshRenderer;
+
+		private float NextBlinkTime;
+		private bool State;
+
+		protected void LateUpdate()
+		{
+			var now = UseScaledTime
+				? Time.time
+				: Time.unscaledTime;
+
+			if (NextBlinkTime < now)
+			{
+				NextBlinkTime += BlinkInterval;
+
+				// Reset timing for huge time gaps.
+				if (NextBlinkTime < now)
+				{
+					NextBlinkTime = now;
+				}
+
+				SwitchMaterial();
+			}
+		}
+
+		public void SwitchMaterial()
+		{
+			State = !State;
+
+			MeshRenderer.sharedMaterial = State
+				? MaterialA
+				: MaterialB;
+		}
+	}
+
+}
