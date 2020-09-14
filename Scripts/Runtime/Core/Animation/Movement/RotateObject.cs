fee4e3e74c89f1ae755e5e5c423467471a170f32
Can Baycay <can.baycay@gmail.com>
2020-09-15 00:15:02 +0300

    Refactor: Move.

---
+++ Scripts/Runtime/Core/Animation/Movement/RotateObject.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+namespace Extenity.AnimationToolbox.ScriptedAnimations
+{
+
+	public class RotateObject : MonoBehaviour
+	{
+		public float RotationSpeed = 1f;
+		public Vector3 RotationAxis = Vector3.up;
+
+		public bool InLocalCoordinates = true;
+		public bool InFixedUpdate = true;
+
+		private void OnEnable()
+		{
+			if (InFixedUpdate)
+			{
+				if (InLocalCoordinates)
+				{
+					Loop.RegisterFixedUpdate(CalculateLocalCoordinates);
+				}
+				else
+				{
+					Loop.RegisterFixedUpdate(CalculateWorldCoordinates);
+				}
+			}
+			else
+			{
+				if (InLocalCoordinates)
+				{
+					Loop.RegisterUpdate(CalculateLocalCoordinates);
+				}
+				else
+				{
+					Loop.RegisterUpdate(CalculateWorldCoordinates);
+				}
+			}
+		}
+
+		private void OnDisable()
+		{
+			if (InFixedUpdate)
+			{
+				if (InLocalCoordinates)
+				{
+					Loop.DeregisterFixedUpdate(CalculateLocalCoordinates);
+				}
+				else
+				{
+					Loop.DeregisterFixedUpdate(CalculateWorldCoordinates);
+				}
+			}
+			else
+			{
+				if (InLocalCoordinates)
+				{
+					Loop.DeregisterUpdate(CalculateLocalCoordinates);
+				}
+				else
+				{
+					Loop.DeregisterUpdate(CalculateWorldCoordinates);
+				}
+			}
+		}
+
+		private void CalculateWorldCoordinates()
+		{
+			transform.Rotate(RotationAxis, RotationSpeed * Loop.DeltaTime, Space.World);
+		}
+
+		private void CalculateLocalCoordinates()
+		{
+			transform.Rotate(RotationAxis, RotationSpeed * Loop.DeltaTime, Space.Self);
+		}
+	}
+
+}
---
+++ Scripts/Runtime/Core/Animation/Rendering/ColorBlinker.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Extenity.AnimationToolbox
+{
+
+	public class ColorBlinker : MonoBehaviour
+	{
+		public float FadeDuration = 0.5f;
+		public Color Color1 = Color.gray;
+		public Color Color2 = Color.white;
+
+		private Color startColor;
+		private Color endColor;
+		private float lastColorChangeTime;
+
+		private Material material;
+
+		protected void Start()
+		{
+			material = GetComponent<Renderer>().material;
+			startColor = Color1;
+			endColor = Color2;
+		}
+
+		protected void Update()
+		{
+			var ratio = (Time.time - lastColorChangeTime) / FadeDuration;
+			ratio = Mathf.Clamp01(ratio);
+			material.color = Color.Lerp(startColor, endColor, Mathf.Sqrt(ratio));
+			//material.color = Color.Lerp(startColor, endColor, ratio * ratio);
+			//material.color = Color.Lerp(startColor, endColor, ratio);
+
+			if (ratio == 1f)
+			{
+				lastColorChangeTime = Time.time;
+
+				// Switch colors
+				var temp = startColor;
+				startColor = endColor;
+				endColor = temp;
+			}
+		}
+	}
+
+}
