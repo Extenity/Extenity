1235041abc97a91b09daa8b860343ffc0a58c7a5
Can Baycay <can.baycay@gmail.com>
2020-10-16 02:45:41 +0300

    Add: Checklist tool.

---
+++ Scripts/Editor/UnityEditor/Editor/Checklist/Checklist.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections.Generic;
+using Extenity.DataToolbox;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Extenity.UnityEditorToolbox
+{
+
+	// TODO: Inspector design:
+	// Ability to add/remove items on the fly. In other words, items are not hardcoded.
+	// Hide details of completed steps and show them as a single line.
+
+	// TODO: Fail the build if the list is not completed. Not every item should fail though.
+
+	[CreateAssetMenu(fileName = "Checklist", menuName = "Checklist")]
+	[HideMonoScript]
+	[Serializable]
+	[ExcludeFromPresetAttribute]
+	[Searchable]
+	public class Checklist : ExtenityScriptableObject<Checklist>
+	{
+		#region Configuration
+
+		protected override string LatestVersion => ChecklistConstants.Version;
+
+		#endregion
+
+		#region Data
+
+		[ListDrawerSettings(Expanded = true)]
+		public List<ChecklistCategory> Categories;
+
+		#endregion
+
+		#region Check If Completed
+
+		public bool CheckIfAllCategoriesAreCompleted()
+		{
+			foreach (var category in Categories)
+			{
+				if (!category.CheckIfAllGroupsAreCompleted())
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public bool CheckIfAllCategoriesAreCompletedOrUnimportant()
+		{
+			foreach (var category in Categories)
+			{
+				if (!category.CheckIfAllGroupsAreCompletedOrUnimportant())
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public bool CheckIfAllCategoriesAreCompletedUntil(string categoryTitle)
+		{
+			foreach (var category in Categories)
+			{
+				if (!category.CheckIfAllGroupsAreCompleted())
+				{
+					return false;
+				}
+				if (category.CategoryTitle.Equals(categoryTitle, StringComparison.InvariantCultureIgnoreCase))
+				{
+					return true;
+				}
+			}
+			Log.Error($"{nameof(Checklist)} category '{categoryTitle}' does not exist.");
+			return false;
+		}
+
+		public bool CheckIfAllCategoriesAreCompletedOrUnimportantUntil(string categoryTitle)
+		{
+			foreach (var category in Categories)
+			{
+				if (!category.CheckIfAllGroupsAreCompletedOrUnimportant())
+				{
+					return false;
+				}
+				if (category.CategoryTitle.Equals(categoryTitle, StringComparison.InvariantCultureIgnoreCase))
+				{
+					return true;
+				}
+			}
+			Log.Error($"{nameof(Checklist)} category '{categoryTitle}' does not exist.");
+			return false;
+		}
+
+		#endregion
+
+		#region Version Migration
+
+		protected override void ApplyMigration(string targetVersion)
+		{
+			switch (Version)
+			{
+				// Example
+				case "0":
+				{
+					// Do the migration here.
+					// MigrationsToUpdateFromVersion0ToVersion1();
+
+					// Mark the settings with resulting migration.
+					Version = "1";
+					break;
+				}
+
+				default:
+					Version = targetVersion;
+					return;
+			}
+
+			// Apply migration over and over until we reach the target version.
+			ApplyMigration(targetVersion);
+		}
+
+		#endregion
+	}
+
+}
---
+++ Scripts/Editor/UnityEditor/Editor/Checklist/ChecklistTypes.cs
@@ -0,0 +1,324 @@
+using System;
+using System.Collections.Generic;
+using Extenity.DataToolbox;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Extenity.UnityEditorToolbox
+{
+
+	public enum ChecklistItemType
+	{
+		Regular = 0,
+		Unimportant = 1,
+	}
+
+	public enum ChecklistItemStatus
+	{
+		NotStarted = 0,
+		InProgress = 1,
+		NeedsAttention = 2,
+		Completed = 3,
+	}
+
+	[Serializable]
+	public class ChecklistItem : ISearchFilterable
+	{
+		[HideInInspector]
+		public string Title;
+
+		[HideInInspector]
+		public string Description;
+
+		[HideInInspector]
+		public ChecklistItemType Type;
+
+		[HideInInspector]
+		public ChecklistItemStatus Status;
+
+		/// <summary>
+		/// Allows entering notes, like the user might enter a note to self when a careful inspection is needed.
+		/// </summary>
+		[HideInInspector]
+		public string Notes;
+
+		#region Initialization
+
+		public ChecklistItem(string title)
+		{
+			Title = title;
+		}
+
+		public ChecklistItem(string title, string description)
+		{
+			Title = title;
+			Description = description;
+		}
+
+		#endregion
+
+		#region Check If Completed
+
+		public bool CheckIfCompleted()
+		{
+			return Status == ChecklistItemStatus.Completed;
+		}
+
+		public bool CheckIfCompletedOrUnimportant()
+		{
+			return CheckIfCompleted() || Type == ChecklistItemType.Unimportant;
+		}
+
+		#endregion
+
+		#region Editor
+
+		[NonSerialized]
+		private bool IsFoldout;
+
+		[OnInspectorGUI]
+		private void DrawStatusIcon()
+		{
+			GUILayout.BeginVertical();
+
+			// Summary line
+			GUILayout.BeginHorizontal();
+			{
+				// Icon
+				{
+					var icon = CheckIfCompletedOrUnimportant()
+						? ChecklistIcons.Texture_Accept
+						: ChecklistIcons.Texture_Reject;
+					if (GUILayout.Button(icon, GUI.skin.GetStyle("Label"), ChecklistConstants.SmallIconLayoutOptions))
+					{
+						IsFoldout = !IsFoldout;
+					}
+				}
+
+				// Status
+				{
+					var rect = GUILayoutUtility.GetRect(112, ChecklistConstants.SmallIconSize, GUILayout.Width(112));
+					rect.y += 2;
+					Status = EnumSelector<ChecklistItemStatus>.DrawEnumField(rect, GUIContent.none, Status, null);
+				}
+
+				// Foldout title
+				{
+					IsFoldout = SirenixEditorGUI.Foldout(IsFoldout, Title);
+				}
+			}
+			GUILayout.EndHorizontal();
+
+			// Details
+			if (IsFoldout || string.IsNullOrWhiteSpace(Title))
+			{
+				GUILayout.Space(6f);
+				GUILayout.BeginHorizontal();
+				GUILayout.Space(ChecklistConstants.SmallIconSize + 8);
+				SirenixEditorGUI.BeginBox();
+				GUILayout.BeginVertical();
+				GUILayout.Space(6f);
+
+				// Title
+				{
+					GUILayout.Label(nameof(Title));
+					Title = GUILayout.TextField(Title);
+				}
+
+				GUILayout.Space(6f);
+
+				// Type
+				{
+					GUILayout.Label(nameof(Type));
+					GUILayout.BeginHorizontal();
+					var rect = GUILayoutUtility.GetRect(90, ChecklistConstants.SmallIconSize, GUILayout.Width(90));
+					rect.y += 2;
+					var newType = EnumSelector<ChecklistItemType>.DrawEnumField(rect, GUIContent.none, Type, null);
+					if (Type == ChecklistItemType.Unimportant)
+					{
+						GUILayout.Space(2f);
+						GUILayout.Label($"'{nameof(ChecklistItemType.Unimportant)}' immediately marks the item as completed regardless of its state.");
+					}
+					Type = newType;
+					GUILayout.EndHorizontal();
+				}
+
+				GUILayout.Space(6f);
+
+				// Description
+				{
+					GUILayout.Label(nameof(Description));
+					var lineCount = Mathf.Max(3, Description.CountLines());
+					var rect = EditorGUILayout.GetControlRect(false, EditorGUIUtility.singleLineHeight * lineCount);
+					Description = EditorGUI.TextArea(rect, Description, EditorStyles.textArea);
+				}
+
+				GUILayout.Space(6f);
+
+				// Notes
+				{
+					GUILayout.Label(nameof(Notes));
+					var lineCount = Mathf.Max(3, Notes.CountLines());
+					var rect = EditorGUILayout.GetControlRect(false, EditorGUIUtility.singleLineHeight * lineCount);
+					Notes = EditorGUI.TextArea(rect, Notes, EditorStyles.textArea);
+				}
+
+				GUILayout.Space(6f);
+				GUILayout.EndVertical();
+				SirenixEditorGUI.EndBox();
+				GUILayout.EndHorizontal();
+			}
+
+			GUILayout.EndVertical();
+		}
+
+		public bool IsMatch(string searchString)
+		{
+			return Description.IndexOf(searchString, StringComparison.InvariantCultureIgnoreCase) >= 0 ||
+			       Notes.IndexOf(searchString, StringComparison.InvariantCultureIgnoreCase) >= 0 ||
+			       Title.IndexOf(searchString, StringComparison.InvariantCultureIgnoreCase) >= 0;
+		}
+
+		#endregion
+	}
+
+	[Serializable]
+	public class ChecklistGroup : ISearchFilterable
+	{
+		[Title("Group", HorizontalLine = false)]
+		[HorizontalGroup("Title")]
+		[HideLabel, PropertySpace(SpaceBefore = 4)]
+		public string GroupTitle;
+
+		[ListDrawerSettings(Expanded = true), PropertySpace(SpaceAfter = 20)]
+		public List<ChecklistItem> Items;
+
+		#region Initialization
+
+		public ChecklistGroup(string title)
+		{
+			GroupTitle = title;
+		}
+
+		#endregion
+
+		#region Check If Completed
+
+		public bool CheckIfAllItemsAreCompleted()
+		{
+			foreach (var item in Items)
+			{
+				if (!item.CheckIfCompleted())
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public bool CheckIfAllItemsAreCompletedOrUnimportant()
+		{
+			foreach (var item in Items)
+			{
+				if (!item.CheckIfCompletedOrUnimportant())
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		#endregion
+
+		#region Editor
+
+		[OnInspectorGUI, PropertySpace(SpaceBefore = 18)]
+		[HorizontalGroup("Title", Width = ChecklistConstants.MidIconSize), PropertyOrder(-1)]
+		private void _DrawIcon()
+		{
+			var icon = CheckIfAllItemsAreCompletedOrUnimportant()
+				? ChecklistIcons.Texture_Accept
+				: ChecklistIcons.Texture_Reject;
+			GUILayout.Label(icon, ChecklistConstants.MidIconLayoutOptions);
+		}
+
+		public bool IsMatch(string searchString)
+		{
+			return GroupTitle.IndexOf(searchString, StringComparison.InvariantCultureIgnoreCase) >= 0;
+		}
+
+		#endregion
+	}
+
+	[Serializable]
+	public class ChecklistCategory : ISearchFilterable
+	{
+		[Title("Category", HorizontalLine = false)]
+		[HorizontalGroup("Title")]
+		[HideLabel, PropertySpace(SpaceBefore = 4)]
+		public string CategoryTitle;
+
+		[ListDrawerSettings(Expanded = true), PropertySpace(SpaceAfter = 20)]
+		public List<ChecklistGroup> Groups;
+
+		#region Initialization
+
+		public ChecklistCategory(string title)
+		{
+			CategoryTitle = title;
+		}
+
+		#endregion
+
+		#region Check If Completed
+
+		public bool CheckIfAllGroupsAreCompleted()
+		{
+			foreach (var group in Groups)
+			{
+				if (!group.CheckIfAllItemsAreCompleted())
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public bool CheckIfAllGroupsAreCompletedOrUnimportant()
+		{
+			foreach (var group in Groups)
+			{
+				if (!group.CheckIfAllItemsAreCompletedOrUnimportant())
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		#endregion
+
+		#region Editor
+
+		[OnInspectorGUI, PropertySpace(SpaceBefore = 12)]
+		[HorizontalGroup("Title", Width = ChecklistConstants.BigIconSize), PropertyOrder(-1)]
+		private void _DrawIcon()
+		{
+			var icon = CheckIfAllGroupsAreCompletedOrUnimportant()
+				? ChecklistIcons.Texture_Accept
+				: ChecklistIcons.Texture_Reject;
+			GUILayout.Label(icon, ChecklistConstants.BigIconLayoutOptions);
+		}
+
+		public bool IsMatch(string searchString)
+		{
+			return CategoryTitle.IndexOf(searchString, StringComparison.InvariantCultureIgnoreCase) >= 0;
+		}
+
+		#endregion
+	}
+
+}
