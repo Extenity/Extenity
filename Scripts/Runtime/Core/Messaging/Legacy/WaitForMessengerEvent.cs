bd984f6067b65c3e6f0dd1342621ff01805a4234
Can Baycay <can.baycay@gmail.com>
2020-10-16 19:01:08 +0300

    Add: WaitForMessengerEvent

---
+++ Scripts/Runtime/Core/Messaging/Legacy/WaitForMessengerEvent.cs
@@ -0,0 +1,34 @@
+#if UseLegacyMessenger
+
+using UnityEngine;
+
+namespace Extenity.MessagingToolbox
+{
+
+	public class WaitForMessengerEvent : CustomYieldInstruction
+	{
+		private string EventName;
+
+		public WaitForMessengerEvent(string eventName)
+		{
+			EventName = eventName;
+			_KeepWaiting = true;
+			Messenger.RegisterEvent(eventName, OnEventEmitted);
+		}
+
+		private void OnEventEmitted()
+		{
+			Messenger.DeregisterEvent(EventName, OnEventEmitted);
+			_KeepWaiting = false;
+		}
+
+		private bool _KeepWaiting;
+		public override bool keepWaiting
+		{
+			get => _KeepWaiting;
+		}
+	}
+
+}
+
+#endif
