2fc469d59466e0c969b9a54f6cc8a2d94ba3caef
Can Baycay <can.baycay@gmail.com>
2020-10-16 15:58:03 +0300

    Add: Panel UI right click menu entry.

---
+++ Scripts/Editor/UI/Panel/PanelInspector.cs
@@ -0,0 +1,20 @@
+using Extenity.UnityEditorToolbox;
+using UnityEditor;
+
+namespace Extenity.UIToolbox.Editor
+{
+
+	public static class PanelInspector
+	{
+		#region Hierarchy Right Click Menu
+
+		[MenuItem(ExtenityMenu.WidgetsContext + "Panel" + ExtenityMenu.WidgetsContextPostfix, priority = 2062)] // Just below the Unity's Panel
+		private static void AddToScene(MenuCommand menuCommand)
+		{
+			UIEditorUtilities.InstantiateUIWidgetFromPrefab("Extenity/Widgets/Panel", menuCommand);
+		}
+
+		#endregion
+	}
+
+}
