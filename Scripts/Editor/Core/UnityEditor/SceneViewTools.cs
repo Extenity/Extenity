9ac6ced9a3af489ce8bef0abacf467971de9b040
Can Baycay <can.baycay@gmail.com>
2021-04-03 09:50:30 +0300

    Add: SceneViewTools.CalculateReasonableInstantiationPointOnGround

--- Scripts/Editor/Core/UnityEditor/SceneViewTools.cs
+++ Scripts/Editor/Core/UnityEditor/SceneViewTools.cs
@@ -1,3 +1,5 @@
+using System;
+using Extenity.MathToolbox;
 using UnityEngine;
 using UnityEditor;
 
@@ -31,6 +33,50 @@ namespace Extenity.UnityEditorToolbox.Editor
 			if (view != null)
 				view.MoveToView(transform);
 		}
+
+		#region Camera
+
+		public static Transform GetCameraTransformOfActiveSceneViewOrDisplayErrorMessageBox()
+		{
+			if (SceneView.lastActiveSceneView &&
+			    SceneView.lastActiveSceneView.camera)
+			{
+				SceneView.lastActiveSceneView.Show();
+				return SceneView.lastActiveSceneView.camera.transform;
+			}
+			if (SceneView.currentDrawingSceneView &&
+			    SceneView.currentDrawingSceneView.camera)
+			{
+				SceneView.currentDrawingSceneView.Show();
+				return SceneView.currentDrawingSceneView.camera.transform;
+			}
+			EditorUtility.DisplayDialog("SceneView Required", "You need to open a SceneView first.", "OK");
+			throw new Exception("You need to open a SceneView first.");
+		}
+
+		#endregion
+
+		#region Instantiation Point
+
+		public static Vector3 CalculateReasonableInstantiationPointOnGround(float groundLevelY = 0f, float resultOffsetY = 0f)
+		{
+			var cameraTransform = GetCameraTransformOfActiveSceneViewOrDisplayErrorMessageBox();
+
+			var direction = cameraTransform.forward.WithY(0);
+			direction = direction.sqrMagnitude < 0.001f
+				? Vector3.zero
+				: direction.normalized;
+
+			var cameraPosition = cameraTransform.position;
+			var cameraHeightFromGround = cameraPosition.y - groundLevelY;
+			var groundPositionBelowCamera = cameraPosition.WithY(groundLevelY);
+			var offsetThroughLookingDirection = direction * cameraHeightFromGround;
+			var result = groundPositionBelowCamera + offsetThroughLookingDirection;
+			result.y += resultOffsetY;
+			return result;
+		}
+
+		#endregion
 	}
 
 }
