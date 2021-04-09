ffe60f1d64a8410744015e9043d0c0019795b09b
Can Baycay <can.baycay@gmail.com>
2021-04-09 17:15:09 +0300

    Add: Renderer.SetSharedMaterials

---
+++ Scripts/Runtime/Core/Rendering/RendererTools.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Extenity.RenderingToolbox
+{
+
+	public static class RendererTools
+	{
+		public static void SetSharedMaterials(this Renderer renderer, List<Material> materials)
+		{
+			if (materials == null || materials.Count == 0)
+			{
+				renderer.sharedMaterial = null;
+			}
+			else if (materials.Count == 1)
+			{
+				renderer.sharedMaterial = materials[0];
+			}
+			else
+			{
+				renderer.sharedMaterials = materials.ToArray();
+			}
+		}
+	}
+
+}
