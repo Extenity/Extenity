8743d05eddf6156ae94424ed607415c31f288cdb
Can Baycay <can.baycay@gmail.com>
2021-05-14 23:02:11 +0300

    Add: LogRep.

---
+++ Scripts/Runtime/Core/Debug/Logging/LogRep.cs
@@ -0,0 +1,77 @@
+using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Extenity.DebugToolbox
+{
+
+	public struct LogRep
+	{
+		#region Setup
+
+		public readonly string Prefix;
+		public readonly Object Context;
+		public bool VerboseLoggingActive;
+
+		#endregion
+
+		#region Initialization
+
+		public LogRep(string prefix, Object context = null)
+		{
+			Prefix = prefix;
+			Context = context;
+			VerboseLoggingActive = false;
+		}
+
+		public LogRep CreateStandardPrefix(string prefix, Object context = null)
+		{
+			return new LogRep($"<b>[{prefix}]</b> ", context);
+		}
+
+		#endregion
+
+		#region Log
+
+		/// <remarks>
+		/// Creating the log message might require some allocations for formatted messages. To avoid the overhead of
+		/// creating the log message, the caller may also check if verbose logging is active via VerboseLoggingActive.
+		/// </remarks>
+		// [Conditional("EnableVerboseLogging")] Nope! Note that LogRep uses its own configuration that is applied separately to each LogRep.
+		public void Verbose(string message)
+		{
+			if (VerboseLoggingActive)
+			{
+				Debug.Log(Prefix + message, Context); // Ignored by Code Correct
+			}
+		}
+
+		public void Info(string message)
+		{
+			Debug.Log(Prefix + message, Context); // Ignored by Code Correct
+		}
+
+		public void Warning(string message)
+		{
+			Debug.LogWarning(Prefix + message, Context); // Ignored by Code Correct
+		}
+
+		public void Error(string message)
+		{
+			Debug.LogError(Prefix + message, Context); // Ignored by Code Correct
+		}
+
+		public void Critical(string message)
+		{
+			Debug.LogException(new Exception(Prefix + message), Context); // Ignored by Code Correct
+		}
+
+		public void Exception(Exception exception)
+		{
+			Debug.LogException(exception, Context); // Ignored by Code Correct
+		}
+
+		#endregion
+	}
+
+}
