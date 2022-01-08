e6ac72af49b0ea5eb3a544d9a62a4022fdc6c53e
Can Baycay <can.baycay@gmail.com>
2022-01-08 10:31:25 +0300

    Add: Some UNITY define blocks for UniversalExtenity project.

--- Scripts/Runtime/Core/UnityEditor/DontShowEditorHandler.cs
+++ Scripts/Runtime/Core/UnityEditor/DontShowEditorHandler.cs
@@ -1,3 +1,5 @@
+#if UNITY
+
 using UnityEngine;
 
 // TODO: This needs to be in Extenity.UnityEditorToolbox namespace. But make sure nothing gets broken. Maybe do this when there are no ongoing critical projects using Extenity.
@@ -9,3 +11,5 @@ namespace Extenity.GameObjectToolbox
 	}
 
 }
+
+#endif
