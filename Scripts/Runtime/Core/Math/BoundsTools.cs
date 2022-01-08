e7985eb0aa6c97a5ca6dde9af6a9cf373f2a24cb
Can Baycay <can.baycay@gmail.com>
2022-01-08 12:28:11 +0300

    Refactor: Moved Bounds related codes into BoundsTools.

--- Scripts/Runtime/Core/Math/BoundsTools.cs
+++ Scripts/Runtime/Core/Math/BoundsTools.cs
@@ -2,12 +2,105 @@
 
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.Mathematics.math;
 
 namespace Extenity.MathToolbox
 {
 
 	public static class BoundsTools
 	{
+		public static readonly Bounds NaN = new Bounds(Vector3Tools.NaN, Vector3Tools.NaN);
+
+		#region Fix and Reset Bounds
+
+		public static void Fix(this Bounds bounds)
+		{
+			Vector3 min = bounds.min;
+			Vector3 max = bounds.max;
+
+			bool switchX = min.x > max.x;
+			bool switchY = min.y > max.y;
+			bool switchZ = min.z > max.z;
+
+			if (switchX)
+			{
+				if (switchY)
+				{
+					if (switchZ)
+					{
+						bounds.SetMinMax(
+							new Vector3(max.x, max.y, max.z),
+							new Vector3(min.x, min.y, min.z));
+					}
+					else
+					{
+						bounds.SetMinMax(
+							new Vector3(max.x, max.y, min.z),
+							new Vector3(min.x, min.y, max.z));
+					}
+				}
+				else
+				{
+					if (switchZ)
+					{
+						bounds.SetMinMax(
+							new Vector3(max.x, min.y, max.z),
+							new Vector3(min.x, max.y, min.z));
+					}
+					else
+					{
+						bounds.SetMinMax(
+							new Vector3(max.x, min.y, min.z),
+							new Vector3(min.x, max.y, max.z));
+					}
+				}
+			}
+			else
+			{
+				if (switchY)
+				{
+					if (switchZ)
+					{
+						bounds.SetMinMax(
+							new Vector3(min.x, max.y, max.z),
+							new Vector3(max.x, min.y, min.z));
+					}
+					else
+					{
+						bounds.SetMinMax(
+							new Vector3(min.x, max.y, min.z),
+							new Vector3(max.x, min.y, max.z));
+					}
+				}
+				else
+				{
+					if (switchZ)
+					{
+						bounds.SetMinMax(
+							new Vector3(min.x, min.y, max.z),
+							new Vector3(max.x, max.y, min.z));
+					}
+					else
+					{
+						bounds.SetMinMax(
+							new Vector3(min.x, min.y, min.z),
+							new Vector3(max.x, max.y, max.z));
+					}
+				}
+			}
+		}
+
+		public static void Reset(this Bounds bounds)
+		{
+			bounds.SetMinMax(
+				new Vector3(float.MaxValue, float.MaxValue, float.MaxValue),
+				new Vector3(float.MinValue, float.MinValue, float.MinValue));
+		}
+
+		#endregion
+
+		#region Calculate Bounds Of Points
+
 		public static Bounds2 CalculateBounds(this IList<Vector2> points)
 		{
 			var minX = points[0].x;
@@ -64,6 +157,51 @@ namespace Extenity.MathToolbox
 			bounds.SetMinMax(new Vector2(minX, minY), new Vector2(maxX, maxY));
 			return bounds;
 		}
+
+		#endregion
+
+		#region Bounds
+
+#if UNITY
+		// Source: http://answers.unity3d.com/questions/361275/cant-convert-bounds-from-world-coordinates-to-loca.html
+		public static Bounds TransformBounds(this Transform transform, Bounds localBounds)
+		{
+			var center = transform.TransformPoint(localBounds.center);
+
+			// transform the local extents' axes
+			var extents = localBounds.extents;
+			var axisX = transform.TransformVector(extents.x, 0, 0);
+			var axisY = transform.TransformVector(0, extents.y, 0);
+			var axisZ = transform.TransformVector(0, 0, extents.z);
+
+			// sum their absolute value to get the world extents
+			extents.x = abs(axisX.x) + abs(axisY.x) + abs(axisZ.x);
+			extents.y = abs(axisX.y) + abs(axisY.y) + abs(axisZ.y);
+			extents.z = abs(axisX.z) + abs(axisY.z) + abs(axisZ.z);
+
+			return new Bounds { center = center, extents = extents };
+		}
+
+		public static Bounds TransformBounds(this Transform transform, Bounds localBounds, Transform relativeTo)
+		{
+			var center = relativeTo.InverseTransformPoint(transform.TransformPoint(localBounds.center));
+
+			// transform the local extents' axes
+			var extents = localBounds.extents;
+			var axisX = relativeTo.InverseTransformVector(transform.TransformVector(extents.x, 0, 0));
+			var axisY = relativeTo.InverseTransformVector(transform.TransformVector(0, extents.y, 0));
+			var axisZ = relativeTo.InverseTransformVector(transform.TransformVector(0, 0, extents.z));
+
+			// sum their absolute value to get the world extents
+			extents.x = abs(axisX.x) + abs(axisY.x) + abs(axisZ.x);
+			extents.y = abs(axisX.y) + abs(axisY.y) + abs(axisZ.y);
+			extents.z = abs(axisX.z) + abs(axisY.z) + abs(axisZ.z);
+
+			return new Bounds { center = center, extents = extents };
+		}
+#endif
+
+		#endregion
 	}
 
 }
