f71af30656d584527564d9272652864756979b83
Can Baycay <can.baycay@gmail.com>
2022-01-16 11:44:20 +0300

    Add: Some new math library conversions.

--- Scripts/Runtime/Core/Math/Rectangle.cs
+++ Scripts/Runtime/Core/Math/Rectangle.cs
@@ -1,61 +1,59 @@
-#if UNITY // TODO-UniversalExtenity: Convert these to Mathematics after importing it into Universal project.
-
 using System.Collections.Generic;
-using UnityEngine;
+using Unity.Mathematics;
+using static Unity.Mathematics.math;
 
 namespace Extenity.MathToolbox
 {
 
 	public struct Rectangle
 	{
-		public Vector2 centerPosition;
-		public Vector2 size;
+		public float2 centerPosition;
+		public float2 size;
 		public float rotation;
-
 
 		#region Extract points to array
 
-		public Vector2[] GetPoints()
+		public float2[] GetPoints()
 		{
-			var points = new Vector2[4];
+			var points = new float2[4];
 			var halfSize = size * 0.5f;
-			points[0] = new Vector2(centerPosition.x, centerPosition.y) + new Vector2(-halfSize.x, -halfSize.y).Rotate(-rotation);
-			points[1] = new Vector2(centerPosition.x, centerPosition.y) + new Vector2(halfSize.x, -halfSize.y).Rotate(-rotation);
-			points[2] = new Vector2(centerPosition.x, centerPosition.y) + new Vector2(halfSize.x, halfSize.y).Rotate(-rotation);
-			points[3] = new Vector2(centerPosition.x, centerPosition.y) + new Vector2(-halfSize.x, halfSize.y).Rotate(-rotation);
+			points[0] = float2(centerPosition.x, centerPosition.y) + float2(-halfSize.x, -halfSize.y).Rotate(-rotation);
+			points[1] = float2(centerPosition.x, centerPosition.y) + float2(halfSize.x, -halfSize.y).Rotate(-rotation);
+			points[2] = float2(centerPosition.x, centerPosition.y) + float2(halfSize.x, halfSize.y).Rotate(-rotation);
+			points[3] = float2(centerPosition.x, centerPosition.y) + float2(-halfSize.x, halfSize.y).Rotate(-rotation);
 			return points;
 		}
 
-		public Vector3[] GetPointsXY(float zValue = 0f)
+		public float3[] GetPointsXY(float zValue = 0f)
 		{
-			var points = new Vector3[4];
+			var points = new float3[4];
 			var halfSize = size * 0.5f;
-			points[0] = new Vector3(centerPosition.x, centerPosition.y, zValue) + new Vector3(-halfSize.x, -halfSize.y, 0f).RotateAroundZ(-rotation);
-			points[1] = new Vector3(centerPosition.x, centerPosition.y, zValue) + new Vector3(halfSize.x, -halfSize.y, 0f).RotateAroundZ(-rotation);
-			points[2] = new Vector3(centerPosition.x, centerPosition.y, zValue) + new Vector3(halfSize.x, halfSize.y, 0f).RotateAroundZ(-rotation);
-			points[3] = new Vector3(centerPosition.x, centerPosition.y, zValue) + new Vector3(-halfSize.x, halfSize.y, 0f).RotateAroundZ(-rotation);
+			points[0] = float3(centerPosition.x, centerPosition.y, zValue) + float3(-halfSize.x, -halfSize.y, 0f).RotateAroundZ(-rotation);
+			points[1] = float3(centerPosition.x, centerPosition.y, zValue) + float3(halfSize.x, -halfSize.y, 0f).RotateAroundZ(-rotation);
+			points[2] = float3(centerPosition.x, centerPosition.y, zValue) + float3(halfSize.x, halfSize.y, 0f).RotateAroundZ(-rotation);
+			points[3] = float3(centerPosition.x, centerPosition.y, zValue) + float3(-halfSize.x, halfSize.y, 0f).RotateAroundZ(-rotation);
 			return points;
 		}
 
-		public Vector3[] GetPointsXZ(float yValue = 0f)
+		public float3[] GetPointsXZ(float yValue = 0f)
 		{
-			var points = new Vector3[4];
+			var points = new float3[4];
 			var halfSize = size * 0.5f;
-			points[0] = new Vector3(centerPosition.x, yValue, centerPosition.y) + new Vector3(-halfSize.x, 0f, -halfSize.y).RotateAroundY(-rotation);
-			points[1] = new Vector3(centerPosition.x, yValue, centerPosition.y) + new Vector3(halfSize.x, 0f, -halfSize.y).RotateAroundY(-rotation);
-			points[2] = new Vector3(centerPosition.x, yValue, centerPosition.y) + new Vector3(halfSize.x, 0f, halfSize.y).RotateAroundY(-rotation);
-			points[3] = new Vector3(centerPosition.x, yValue, centerPosition.y) + new Vector3(-halfSize.x, 0f, halfSize.y).RotateAroundY(-rotation);
+			points[0] = float3(centerPosition.x, yValue, centerPosition.y) + float3(-halfSize.x, 0f, -halfSize.y).RotateAroundY(-rotation);
+			points[1] = float3(centerPosition.x, yValue, centerPosition.y) + float3(halfSize.x, 0f, -halfSize.y).RotateAroundY(-rotation);
+			points[2] = float3(centerPosition.x, yValue, centerPosition.y) + float3(halfSize.x, 0f, halfSize.y).RotateAroundY(-rotation);
+			points[3] = float3(centerPosition.x, yValue, centerPosition.y) + float3(-halfSize.x, 0f, halfSize.y).RotateAroundY(-rotation);
 			return points;
 		}
 
-		public Vector3[] GetPointsYZ(float xValue = 0f)
+		public float3[] GetPointsYZ(float xValue = 0f)
 		{
-			var points = new Vector3[4];
+			var points = new float3[4];
 			var halfSize = size * 0.5f;
-			points[0] = new Vector3(xValue, centerPosition.x, centerPosition.y) + new Vector3(0f, -halfSize.x, -halfSize.y).RotateAroundX(-rotation);
-			points[1] = new Vector3(xValue, centerPosition.x, centerPosition.y) + new Vector3(0f, halfSize.x, -halfSize.y).RotateAroundX(-rotation);
-			points[2] = new Vector3(xValue, centerPosition.x, centerPosition.y) + new Vector3(0f, halfSize.x, halfSize.y).RotateAroundX(-rotation);
-			points[3] = new Vector3(xValue, centerPosition.x, centerPosition.y) + new Vector3(0f, -halfSize.x, halfSize.y).RotateAroundX(-rotation);
+			points[0] = float3(xValue, centerPosition.x, centerPosition.y) + float3(0f, -halfSize.x, -halfSize.y).RotateAroundX(-rotation);
+			points[1] = float3(xValue, centerPosition.x, centerPosition.y) + float3(0f, halfSize.x, -halfSize.y).RotateAroundX(-rotation);
+			points[2] = float3(xValue, centerPosition.x, centerPosition.y) + float3(0f, halfSize.x, halfSize.y).RotateAroundX(-rotation);
+			points[3] = float3(xValue, centerPosition.x, centerPosition.y) + float3(0f, -halfSize.x, halfSize.y).RotateAroundX(-rotation);
 			return points;
 		}
 
@@ -68,7 +66,7 @@ namespace Extenity.MathToolbox
 			return CheckRectRectIntersection(rect1.GetPoints(), rect2.GetPoints());
 		}
 
-		public static bool CheckRectRectIntersection(Vector2[] rect1, Vector2[] rect2)
+		public static bool CheckRectRectIntersection(float2[] rect1, float2[] rect2)
 		{
 			if (DoAxisSeparationTest(rect1[0], rect1[1], rect1[2], rect2)) return false;
 			if (DoAxisSeparationTest(rect1[0], rect1[3], rect1[2], rect2)) return false;
@@ -90,10 +88,10 @@ namespace Extenity.MathToolbox
 		/// <param name="otherQuadPoints">The four points of the other quad.</param>
 		/// <returns>Returns <c>true</c>, if the specified edge is a separating axis (and the quadrilaterals therefor don't 
 		/// intersect). Returns <c>false</c>, if it's not a separating axis.</returns>
-		private static bool DoAxisSeparationTest(Vector2 x1, Vector2 x2, Vector2 x3, IEnumerable<Vector2> otherQuadPoints)
+		private static bool DoAxisSeparationTest(float2 x1, float2 x2, float2 x3, IEnumerable<float2> otherQuadPoints)
 		{
-			Vector2 vec = x2 - x1;
-			Vector2 rotated = new Vector2(-vec.y, vec.x);
+			float2 vec = x2 - x1;
+			float2 rotated = new float2(-vec.y, vec.x);
 
 			bool refSide = (rotated.x * (x3.x - x1.x)
 						  + rotated.y * (x3.y - x1.y)) >= 0;
@@ -119,5 +117,3 @@ namespace Extenity.MathToolbox
 	}
 
 }
-
-#endif
