6d1e6df47f6b5d2ff8c58decfcd0645de51dd1a8
Yiğitcan Öksüz <yigitcanoksuz@panteon.games>
2022-02-02 19:24:59 +0300

    Add:Stash and re-apply test class.

---
+++ Tests/Editor/Core/Build/Test_StashGitChangesTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Extenity.FileSystemToolbox;
+using NUnit.Framework;
+using UnityEngine;
+namespace ExtenityTests.Build
+{
+    public class Test_StashGitChangesTests
+    {
+        private static readonly string _dummyRepoPath = Path.Combine(Application.persistentDataPath,"DummyRepo");
+        
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            
+        }
+
+        private void CreateDummyRepo()
+        {
+            if (!Directory.Exists(_dummyRepoPath))
+            {
+                Directory.CreateDirectory(_dummyRepoPath);
+            }
+        }
+
+        private void CreateDummyFiles()
+        {
+            
+        }
+
+        private void AddSubmoduleToTheDummyRepo()
+        {
+            
+        }
+
+        private void DeleteDummyRepoFolder()
+        {
+            
+        }
+    }
+}
