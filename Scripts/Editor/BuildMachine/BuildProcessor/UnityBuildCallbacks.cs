1b71c6716fd307e039c528380fd0a7512478c820
Can Baycay <can.baycay@gmail.com>
2023-05-11 08:22:51 +0300

    Change: Removed Development and Release build info in build callback logs.

--- Scripts/Editor/BuildMachine/BuildProcessor/UnityBuildCallbacks.cs
+++ Scripts/Editor/BuildMachine/BuildProcessor/UnityBuildCallbacks.cs
@@ -1,5 +1,4 @@
 using Extenity.BuildToolbox.Editor;
-using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 
@@ -14,15 +13,7 @@ namespace Extenity.BuildMachine.Editor
 
 		public void OnPreprocessBuild(BuildReport report)
 		{
-#if Release
-			var release = true;
-#else
-			var release = false;
-#endif
-			//var unityDebug = Debug.isDebugBuild; This does not say what option the build was started. Only tells if the Development option is ticked in Build Settings window.
-			var unityDebug = report.summary.options.HasFlag(BuildOptions.Development);
-
-			Log.Info($"BuildMachine checking in at preprocess callback (Release: {release}, UnityDev: {unityDebug})... Report details: " + report.ToDetailedLogString());
+			Log.Info("BuildMachine checking in at preprocess callback. BuildReport details: " + report.ToDetailedLogString());
 
 			// See 713951791.
 			//EditorSceneManagerTools.EnforceUserToSaveAllModifiedScenes("First you need to save the scene before building."); Disabled because it causes an internal Unity error at build time.
@@ -30,7 +21,7 @@ namespace Extenity.BuildMachine.Editor
 
 		public void OnPostprocessBuild(BuildReport report)
 		{
-			Log.Info($"BuildMachine checking in at postprocess callback... Report details: " + report.ToDetailedLogString());
+			Log.Info("BuildMachine checking in at postprocess callback. BuildReport details: " + report.ToDetailedLogString());
 		}
 
 		#region Log
