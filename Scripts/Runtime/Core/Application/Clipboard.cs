3bc10a559016721c9c1131850d40ebcc15b58062
Can Baycay <can.baycay@gmail.com>
2023-09-03 08:37:17 +0300

    Change: Using GUIUtility.systemCopyBuffer instead of calling system-specific clipboard methods.

--- Scripts/Runtime/Core/Application/Clipboard.cs
+++ Scripts/Runtime/Core/Application/Clipboard.cs
@@ -1,41 +1,23 @@
+using UnityEngine;
 
 namespace Extenity.ApplicationToolbox
 {
 
-	public abstract class Clipboard
+	public static class Clipboard
 	{
-		#region Singleton
-
-		private static Clipboard _Instance;
-		public static Clipboard Instance
-		{
-			get
-			{
-				if (_Instance == null)
-					throw new InternalException(113857182); // Clipboard system is not initialized yet
-				return _Instance;
-			}
-			protected set
-			{
-				if (_Instance != null)
-					throw new InternalException(112857182); // There already was an existing Clipboard system running
-				_Instance = value;
-			}
-		}
-
-		#endregion
-
-		protected abstract void DoSetClipboardText(string text, bool log);
-		protected abstract string DoGetClipboardText();
-
 		public static void SetClipboardText(string text, bool log)
 		{
-			Instance.DoSetClipboardText(text, log);
+			GUIUtility.systemCopyBuffer = text;
+
+			if (log)
+			{
+				Log.With("Clipboard").Info("Copied to clipboard: " + text);
+			}
 		}
 
 		public static string GetClipboardText()
 		{
-			return Instance.DoGetClipboardText();
+			return GUIUtility.systemCopyBuffer;
 		}
 	}
 
