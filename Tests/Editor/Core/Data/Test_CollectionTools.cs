c11267a1f62ed63a0491bdadc939d6f5deaaef65
Can Baycay <can.baycay@gmail.com>
2024-09-08 21:40:34 +0300

    Refactor: Code readability.

--- Tests/Editor/Core/Data/Test_CollectionTools.cs
+++ Tests/Editor/Core/Data/Test_CollectionTools.cs
@@ -11,7 +11,7 @@ namespace ExtenityTests.DataToolbox
 		#region Array / List / Collection / Enumerable
 
 		[Test]
-		public static void ArrayInsert()
+		public void Array_Insert()
 		{
 			int[] array = null;
 			Assert.Throws<NullReferenceException>(() => array.Insert(0, 84, out array));
@@ -20,41 +20,40 @@ namespace ExtenityTests.DataToolbox
 			Assert.Throws<ArgumentOutOfRangeException>(() => array.Insert(-1, 84, out array));
 			Assert.Throws<ArgumentOutOfRangeException>(() => array.Insert(1, 84, out array));
 			array.Insert(0, 84, out array);
-			Assert.True(array.Length == 1);
-			Assert.True(array[0] == 84);
+			AssertArray(array, 84);
 
 			array = new int[] { 42 };
 			array.Insert(0, 84, out array);
-			Assert.True(array.Length == 2);
-			Assert.True(array[0] == 84);
-			Assert.True(array[1] == 42);
+			AssertArray(array, 84, 42);
 
 			array = new int[] { 42 };
 			array.Insert(1, 84, out array);
-			Assert.True(array.Length == 2);
-			Assert.True(array[0] == 42);
-			Assert.True(array[1] == 84);
+			AssertArray(array, 42, 84);
 
 			array = new int[] { 21, 42 };
 			array.Insert(0, 84, out array);
-			Assert.True(array.Length == 3);
-			Assert.True(array[0] == 84);
-			Assert.True(array[1] == 21);
-			Assert.True(array[2] == 42);
+			AssertArray(array, 84, 21, 42);
 
 			array = new int[] { 21, 42 };
 			array.Insert(1, 84, out array);
-			Assert.True(array.Length == 3);
-			Assert.True(array[0] == 21);
-			Assert.True(array[1] == 84);
-			Assert.True(array[2] == 42);
+			AssertArray(array, 21, 84, 42);
 
 			array = new int[] { 21, 42 };
 			array.Insert(2, 84, out array);
-			Assert.True(array.Length == 3);
-			Assert.True(array[0] == 21);
-			Assert.True(array[1] == 42);
-			Assert.True(array[2] == 84);
+			AssertArray(array, 21, 42, 84);
+		}
+
+		#endregion
+
+		#region Utilities
+
+		private static void AssertArray<T>(T[] array, params T[] expected)
+		{
+			Assert.AreEqual(expected.Length, array.Length);
+			for (int i = 0; i < expected.Length; i++)
+			{
+				Assert.AreEqual(expected[i], array[i]);
+			}
 		}
 
 		#endregion
