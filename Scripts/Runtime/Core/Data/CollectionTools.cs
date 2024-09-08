1ab9ab936339c41efd7a6358dd6ddbb6bb83d927
Can Baycay <can.baycay@gmail.com>
2024-09-08 21:42:11 +0300

    Optimization: Removed redundant checks in List.AddSorted, after seeing that tests are still green.

--- Scripts/Runtime/Core/Data/CollectionTools.cs
+++ Scripts/Runtime/Core/Data/CollectionTools.cs
@@ -224,22 +224,7 @@ namespace Extenity.DataToolbox
 
 		public static void AddSorted<T>(this List<T> list, in T item) where T : IComparable<T>
 		{
-			if (list.Count == 0)
-			{
-				list.Add(item);
-				return;
-			}
-			if (list[list.Count - 1].CompareTo(item) <= 0)
-			{
-				list.Add(item);
-				return;
-			}
-			if (list[0].CompareTo(item) >= 0)
-			{
-				list.Insert(0, item);
-				return;
-			}
-			int index = list.BinarySearch(item);
+			var index = list.BinarySearch(item);
 			if (index < 0)
 				index = ~index;
 			list.Insert(index, item);
