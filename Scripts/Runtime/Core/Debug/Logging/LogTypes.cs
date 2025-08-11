e87766451b3ab3102a0d470a3433a2f45b33befb
Can Baycay <can.baycay@gmail.com>
2025-08-11 12:38:43 +0300

    Change: LogSeverity enum value starts from 1, leaving 0 as a default value.

--- Scripts/Runtime/Core/Debug/Logging/LogTypes.cs
+++ Scripts/Runtime/Core/Debug/Logging/LogTypes.cs
@@ -3,11 +3,11 @@ namespace Extenity
 
 	public enum LogSeverity
 	{
-		Verbose,
-		Info,
-		Warning,
-		Error,
-		Fatal,
+		Verbose = 1,
+		Info = 2,
+		Warning = 3,
+		Error = 4,
+		Fatal = 5,
 	}
 
 }
