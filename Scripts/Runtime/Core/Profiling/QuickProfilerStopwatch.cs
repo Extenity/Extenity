f73ae2ec178c0fde8ad870966ee308ddff35e2db
Can Baycay <can.baycay@gmail.com>
2026-05-31 23:09:13 +0300

    Add: QuickProfilerStopwatch now has an optional log cap count.

--- Scripts/Runtime/Core/Profiling/QuickProfilerStopwatch.cs
+++ Scripts/Runtime/Core/Profiling/QuickProfilerStopwatch.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Cysharp.Text;
 using Extenity.DataToolbox;
 using IDisposable = System.IDisposable;
@@ -13,29 +14,30 @@ namespace Extenity.ProfilingToolbox
 		private readonly float ThresholdDurationToConsiderLogging;
 		private readonly LogSeverity LogSeverityAboveThreshold;
 		private readonly LogSeverity LogSeverityBelowThreshold;
+		private readonly int CapLogCountAt;
 		private bool HasThresholdDuration => ThresholdDurationToConsiderLogging > 0f;
 
-		public static QuickProfilerStopwatch WithLog(string profilerTitle, LogSeverity logSeverity = LogSeverity.Info)
+		public static QuickProfilerStopwatch WithLog(string profilerTitle, LogSeverity logSeverity = LogSeverity.Info, int capLogCountAt = 0)
 		{
-			return new(new Logger("Profiling"), profilerTitle, 0, logSeverity, LogSeverity.None);
+			return new(new Logger("Profiling"), profilerTitle, 0, logSeverity, LogSeverity.None, capLogCountAt);
 		}
 
-		public static QuickProfilerStopwatch WithLog(Logger logger, string profilerTitle, LogSeverity logSeverity = LogSeverity.Info)
+		public static QuickProfilerStopwatch WithLog(Logger logger, string profilerTitle, LogSeverity logSeverity = LogSeverity.Info, int capLogCountAt = 0)
 		{
-			return new(logger, profilerTitle, 0, logSeverity, LogSeverity.None);
+			return new(logger, profilerTitle, 0, logSeverity, LogSeverity.None, capLogCountAt);
 		}
 
-		public static QuickProfilerStopwatch WithThreshold(string profilerTitle, float thresholdDurationToConsiderLogging, LogSeverity logSeverityAboveThreshold = LogSeverity.Warning, LogSeverity logSeverityBelowThreshold = LogSeverity.None)
+		public static QuickProfilerStopwatch WithThreshold(string profilerTitle, float thresholdDurationToConsiderLogging, LogSeverity logSeverityAboveThreshold = LogSeverity.Warning, LogSeverity logSeverityBelowThreshold = LogSeverity.None, int capLogCountAt = 0)
 		{
-			return new(new Logger("Profiling"), profilerTitle, thresholdDurationToConsiderLogging, logSeverityAboveThreshold, logSeverityBelowThreshold);
+			return new(new Logger("Profiling"), profilerTitle, thresholdDurationToConsiderLogging, logSeverityAboveThreshold, logSeverityBelowThreshold, capLogCountAt);
 		}
 
-		public static QuickProfilerStopwatch WithThreshold(Logger logger, string profilerTitle, float thresholdDurationToConsiderLogging, LogSeverity logSeverityAboveThreshold = LogSeverity.Warning, LogSeverity logSeverityBelowThreshold = LogSeverity.None)
+		public static QuickProfilerStopwatch WithThreshold(Logger logger, string profilerTitle, float thresholdDurationToConsiderLogging, LogSeverity logSeverityAboveThreshold = LogSeverity.Warning, LogSeverity logSeverityBelowThreshold = LogSeverity.None, int capLogCountAt = 0)
 		{
-			return new(logger, profilerTitle, thresholdDurationToConsiderLogging, logSeverityAboveThreshold, logSeverityBelowThreshold);
+			return new(logger, profilerTitle, thresholdDurationToConsiderLogging, logSeverityAboveThreshold, logSeverityBelowThreshold, capLogCountAt);
 		}
 
-		private QuickProfilerStopwatch(Logger logger, string profilerTitle, float thresholdDurationToConsiderLogging, LogSeverity logSeverityAboveThreshold = LogSeverity.Warning, LogSeverity logSeverityBelowThreshold = LogSeverity.None)
+		private QuickProfilerStopwatch(Logger logger, string profilerTitle, float thresholdDurationToConsiderLogging, LogSeverity logSeverityAboveThreshold = LogSeverity.Warning, LogSeverity logSeverityBelowThreshold = LogSeverity.None, int capLogCountAt = 0)
 		{
 			Stopwatch = new ProfilerStopwatch();
 			Logger = logger;
@@ -43,6 +45,7 @@ namespace Extenity.ProfilingToolbox
 			ThresholdDurationToConsiderLogging = thresholdDurationToConsiderLogging;
 			LogSeverityAboveThreshold = logSeverityAboveThreshold;
 			LogSeverityBelowThreshold = logSeverityBelowThreshold;
+			CapLogCountAt = capLogCountAt;
 
 			Stopwatch.Start();
 		}
@@ -55,18 +58,55 @@ namespace Extenity.ProfilingToolbox
 			{
 				if (Stopwatch.Elapsed > ThresholdDurationToConsiderLogging)
 				{
-					Logger.Any(LogSeverityAboveThreshold, ZString.Concat("Running '", ProfilerTitle, "' took '", Stopwatch.Elapsed.ToStringMinutesSecondsMicrosecondsFromSeconds(), "' which is longer than the expected '", ThresholdDurationToConsiderLogging, "' seconds"));
+					if (ConsumeLogBudgetAndDecideIfShouldLog(LogSeverityAboveThreshold))
+					{
+						Logger.Any(LogSeverityAboveThreshold, ZString.Concat("Running '", ProfilerTitle, "' took '", Stopwatch.Elapsed.ToStringMinutesSecondsMicrosecondsFromSeconds(), "' which is longer than the expected '", ThresholdDurationToConsiderLogging, "' seconds"));
+					}
 				}
-				else
+				else if (ConsumeLogBudgetAndDecideIfShouldLog(LogSeverityBelowThreshold))
 				{
 					Logger.Any(LogSeverityBelowThreshold, ZString.Concat("Running '", ProfilerTitle, "' took '", Stopwatch.Elapsed.ToStringMinutesSecondsMicrosecondsFromSeconds(), "'"));
 				}
 			}
 			else // No threshold duration specified, so we log everything.
 			{
-				Logger.Any(LogSeverityAboveThreshold, ZString.Concat("Running '", ProfilerTitle, "' took '", Stopwatch.Elapsed.ToStringMinutesSecondsMicrosecondsFromSeconds(), "'"));
+				if (ConsumeLogBudgetAndDecideIfShouldLog(LogSeverityAboveThreshold))
+				{
+					Logger.Any(LogSeverityAboveThreshold, ZString.Concat("Running '", ProfilerTitle, "' took '", Stopwatch.Elapsed.ToStringMinutesSecondsMicrosecondsFromSeconds(), "'"));
+				}
 			}
 		}
+
+		#region Log Budget
+
+		private static readonly Dictionary<string, int> LogCountsByTitle = new();
+
+		private bool ConsumeLogBudgetAndDecideIfShouldLog(LogSeverity severity)
+		{
+			if (severity == LogSeverity.None)
+			{
+				return false;
+			}
+
+			if (CapLogCountAt <= 0)
+			{
+				return true;
+			}
+
+			lock (LogCountsByTitle)
+			{
+				var count = LogCountsByTitle.GetValueOrDefault(ProfilerTitle, 0);
+				if (count < CapLogCountAt)
+				{
+					LogCountsByTitle[ProfilerTitle] = count + 1;
+					return true;
+				}
+
+				return false;
+			}
+		}
+
+		#endregion
 	}
 
 }
