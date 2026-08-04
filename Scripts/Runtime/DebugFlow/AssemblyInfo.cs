79810502a8c1048026054da2abc063f8349a567a
Can Baycay <can.baycay@gmail.com>
2026-08-04 21:29:28 +0300

    Removed obsolete assets and codes.

--- Scripts/Runtime/DebugFlow/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using Extenity.CodingToolbox;
-
-[assembly: EnsuredNamespace("Extenity.DebugFlow")]
-
-#if UNITY_EDITOR
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Extenity.DebugFlow.Editor")]
-#endif
